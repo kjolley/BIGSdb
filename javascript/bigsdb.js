0033b6a2d40ee90bd35339fc7a64bbda253e5d25
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2011-02-04 15:53:08 +0000

    Last commit somehow changed the getCookie javascript function.

--- javascript/bigsdb.js
+++ javascript/bigsdb.js
@@ -52,8 +52,7 @@ function getCookie(name) {
   var dc = document.cookie;
   var prefix = name + "=";
   var begin = dc.indexOf("; " + prefix);
-  if (begin == 
-	  1) {
+  if (begin == -1) {
     begin = dc.indexOf(prefix);
     if (begin != 0) return null;
   } else
