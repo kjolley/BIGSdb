3d9ea962cf5608f32042909c94cd80d4be770ef1
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2011-11-10 08:52:42 +0000

    Fix XHTML.

--- lib/BIGSdb/AlleleInfoPage.pm
+++ lib/BIGSdb/AlleleInfoPage.pm
@@ -37,7 +37,7 @@ sub print_content {
 	}
 	my $locus_info = $self->{'datastore'}->get_locus_info($locus);
 	my $cleaned_locus = $self->clean_locus($locus);
-	print "<h1>Allele information" . (defined $allele_id ? " - $cleaned_locus: $allele_id</h1>\n" : '') . "</h1>\n";
+	print "<h1>Allele information" . (defined $allele_id ? " - $cleaned_locus: $allele_id\n" : '') . "</h1>\n";
 	if ( $self->{'system'}->{'dbtype'} eq 'isolates' ) {
 		print "<div class=\"box\" id=\"statusbad\"><p>This function is not available from an isolate database.</p></div>\n";
 		return;
