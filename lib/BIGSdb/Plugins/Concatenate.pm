87b146ba3373b1f87d7617b4112955e394e51f16
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2012-08-03 11:10:46 +0100

    Concatenate plugin: Remove commented out code.

--- lib/BIGSdb/Plugins/Concatenate.pm
+++ lib/BIGSdb/Plugins/Concatenate.pm
@@ -93,24 +93,6 @@ sub run {
 		$pk = $pk_ref->[0];
 	}
 	my $list = $self->get_id_list($pk, $query_file);
-#	if ( $q->param('list') ) {
-#		foreach ( split /\n/, $q->param('list') ) {
-#			chomp;
-#			push @$list, $_;
-#		}
-#	} elsif ($query_file) {
-#		my $qry_ref = $self->get_query($query_file);
-#		return if ref $qry_ref ne 'SCALAR';
-#		return if !$self->create_temp_tables($qry_ref);
-#		if ( $self->{'system'}->{'dbtype'} eq 'isolates' ) {
-#			my $view = $self->{'system'}->{'view'};
-#			$$qry_ref =~ s/SELECT ($view\.\*|\*)/SELECT $pk/;
-#			$self->rewrite_query_ref_order_by($qry_ref) if $self->{'system'}->{'dbtype'} eq 'isolates';
-#		}
-#		$list = $self->{'datastore'}->run_list_query($$qry_ref);
-#	} else {
-#		$list = \@;
-#	}
 	if ( $q->param('submit') ) {
 		my $loci_selected = $self->get_selected_loci;
 		my $scheme_ids    = $self->{'datastore'}->run_list_query("SELECT id FROM schemes");
