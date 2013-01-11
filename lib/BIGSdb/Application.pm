61013f7c01119dbefed853123bbd640582a78fdb
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2013-01-11 14:57:29 +0000

    Update copyright year on Application.pm.

--- lib/BIGSdb/Application.pm
+++ lib/BIGSdb/Application.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#(c) 2010-2012, University of Oxford
+#(c) 2010-2013, University of Oxford
 #E-mail: keith.jolley@zoo.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
