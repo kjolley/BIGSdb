d76c7001b15059450f51f53c737b5dbf289aa938
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2013-06-13 10:20:03 +0100

    Display contig length on allele sequence info page. Use dynamic columnizing of contig info.

--- lib/BIGSdb/AlleleSequencePage.pm
+++ lib/BIGSdb/AlleleSequencePage.pm
@@ -25,6 +25,23 @@ use Log::Log4perl qw(get_logger);
 use Error qw(:try);
 my $logger = get_logger('BIGSdb.Page');
 
+sub initiate {
+	my ($self) = @_;
+	$self->{$_} = 1 foreach qw(jQuery jQuery.columnizer);
+	return;
+}
+
+sub get_javascript {
+	my ($self) = @_;
+	my $buffer = << "END";
+\$(function () {
+	\$(".data").columnize({width:400});
+});
+
+END
+	return $buffer;
+}
+
 sub print_content {
 	my ($self)     = @_;
 	my $q          = $self->{'cgi'};
@@ -50,31 +67,34 @@ sub print_content {
 	print " (@name)" if $name[1];
 	say "</h1>";
 	my $flanking = $self->{'prefs'}->{'flanking'};
-	my $qry      = "SELECT seqbin_id,start_pos,end_pos,reverse,complete,method FROM allele_sequences LEFT JOIN sequence_bin ON "
-	  . "allele_sequences.seqbin_id = sequence_bin.id WHERE isolate_id=? AND locus=? ORDER BY complete desc,allele_sequences.datestamp";
+	my $qry =
+	    "SELECT seqbin_id,start_pos,end_pos,reverse,complete,method,length(sequence) AS seqlength FROM allele_sequences LEFT "
+	  . "JOIN sequence_bin ON allele_sequences.seqbin_id = sequence_bin.id WHERE isolate_id=? AND locus=? ORDER BY "
+	  . "complete desc,allele_sequences.datestamp";
 	my $sql = $self->{'db'}->prepare($qry);
 	eval { $sql->execute( $isolate_id, $locus ) };
 	$logger->error($@) if $@;
 	my $buffer;
 	my $update_buffer = '';
 
-	while ( my ( $seqbin_id, $start_pos, $end_pos, $reverse, $complete, $method ) = $sql->fetchrow_array ) {
+	while ( my ( $seqbin_id, $start_pos, $end_pos, $reverse, $complete, $method, $seqlength ) = $sql->fetchrow_array ) {
 		$buffer .= "<dl class=\"data\">";
-		$buffer .= "<dt>sequence bin id</dt><dd>$seqbin_id</dd>";
+		$buffer .= "<dt class=\"dontend\">sequence bin id</dt><dd>$seqbin_id</dd>";
+		$buffer .= "<dt class=\"dontend\">contig length</dt><dd>$seqlength</dd>";
 		if ( $self->{'curate'} ) {
 			$update_buffer = " <a href=\"$self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=tagUpdate&amp;seqbin_id="
 			  . "$seqbin_id&amp;locus=$locus&amp;start_pos=$start_pos&amp;end_pos=$end_pos\" class=\"smallbutton\">Update</a>\n";
 		}
 		my $translate = ( $locus_info->{'coding_sequence'} || $locus_info->{'data_type'} eq 'peptide' ) ? 1 : 0;
 		my $orf = $locus_info->{'orf'} || 1;
-		$buffer .= "<dt>start</dt><dd>$start_pos</dd>\n";
-		$buffer .= "<dt>end</dt><dd>$end_pos</dd>\n";
+		$buffer .= "<dt class=\"dontend\">start</dt><dd>$start_pos</dd>\n";
+		$buffer .= "<dt class=\"dontend\">end</dt><dd>$end_pos</dd>\n";
 		my $length = abs( $end_pos - $start_pos + 1 );
-		$buffer .= "<dt>length</dt><dd>$length</dd>\n";
+		$buffer .= "<dt class=\"dontend\">length</dt><dd>$length</dd>\n";
 		my $orientation = $reverse ? 'reverse' : 'forward';
-		$buffer .= "<dt>orientation</dt><dd>$orientation</dd>\n";
-		$buffer .= "<dt>complete</dt><dd>" . ( $complete ? 'yes' : 'no' ) . "</dd>\n";
-		$buffer .= "<dt>method</dt><dd>$method</dd>\n";
+		$buffer .= "<dt class=\"dontend\">orientation</dt><dd>$orientation</dd>\n";
+		$buffer .= "<dt class=\"dontend\">complete</dt><dd>" . ( $complete ? 'yes' : 'no' ) . "</dd>\n";
+		$buffer .= "<dt class=\"dontend\">method</dt><dd>$method</dd>\n";
 		my $display = $self->format_seqbin_sequence(
 			{ seqbin_id => $seqbin_id, reverse => $reverse, start => $start_pos, end => $end_pos, translate => $translate, orf => $orf } );
 		$buffer .= "</dl>";
