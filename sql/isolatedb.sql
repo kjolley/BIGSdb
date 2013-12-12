fd4c8124ceee5ba6d31443a8f13c6a379c6bf4c6
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2013-12-12 11:08:24 +0000

    Create database tables for adding attributes to sequence bin records.

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -292,6 +292,40 @@ ON UPDATE CASCADE
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON accession TO apache;
 
+CREATE TABLE sequence_attributes (
+key text NOT NULL,
+type text NOT NULL,
+description text,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (key),
+CONSTRAINT sa_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON sequence_attributes TO apache;
+
+CREATE TABLE sequence_attribute_values (
+seqbin_id integer NOT NULL,
+key text NOT NULL,
+value text NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (seqbin_id,key),
+CONSTRAINT sav_seqbin FOREIGN KEY (seqbin_id) REFERENCES sequence_bin
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT sav_key FOREIGN KEY (key) REFERENCES sequence_attributes
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT sav_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON sequence_attribute_values TO apache;
+
 CREATE TABLE refs (
 isolate_id integer NOT NULL,
 pubmed_id integer NOT NULL,
--- sql/upgrade/isolatedb_v.1.7.sql
+++ sql/upgrade/isolatedb_v.1.7.sql
@@ -7,3 +7,36 @@ DROP INDEX i_id;
 
 ALTER TABLE loci DROP COLUMN description;
 
+CREATE TABLE sequence_attributes (
+key text NOT NULL,
+type text NOT NULL,
+description text,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (key),
+CONSTRAINT sa_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON sequence_attributes TO apache;
+
+CREATE TABLE sequence_attribute_values (
+seqbin_id integer NOT NULL,
+key text NOT NULL,
+value text NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (seqbin_id,key),
+CONSTRAINT sav_seqbin FOREIGN KEY (seqbin_id) REFERENCES sequence_bin
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT sav_key FOREIGN KEY (key) REFERENCES sequence_attributes
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT sav_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON sequence_attribute_values TO apache;
