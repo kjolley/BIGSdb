a06e1fdd797c161dbe5fb64703b6022763a8f216
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2014-05-11 09:22:28 +0100

    Add long option names for autotag and scannew scripts.

--- scripts/automation/autotag.pl
+++ scripts/automation/autotag.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -T
 #Automatically tag scan genomes for exactly matching alleles
 #Written by Keith Jolley
-#Copyright (c) 2011, University of Oxford
+#Copyright (c) 2011-2014, University of Oxford
 #E-mail: keith.jolley@zoo.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -18,9 +18,9 @@
 #
 #You should have received a copy of the GNU General Public License
 #along with BIGSdb.  If not, see <http://www.gnu.org/licenses/>.
-
 use strict;
 use warnings;
+use 5.010;
 ###########Local configuration################################
 use constant {
 	CONFIG_DIR       => '/etc/bigsdb',
@@ -33,23 +33,42 @@ use constant {
 };
 #######End Local configuration################################
 use lib (LIB_DIR);
-use Getopt::Std;
+use Getopt::Long qw(:config no_ignore_case);
 use BIGSdb::Offline::AutoTag;
-
 my %opts;
-getopts( 'd:i:I:l:m:p:P:R:s:t:w:x:y:0hnoqrT', \%opts );
+GetOptions(
+	'd|database=s'         => \$opts{'d'},
+	'i|isolates=s'         => \$opts{'i'},
+	'I|exclude_isolates=s' => \$opts{'I'},
+	'l|loci=s'             => \$opts{'l'},
+	'L|exclude_loci=s'     => \$opts{'L'},
+	'm|min_size=i'         => \$opts{'m'},
+	'p|projects=s'         => \$opts{'p'},
+	'P|exclude_projects=s' => \$opts{'P'},
+	'R|locus_regex=s'      => \$opts{'R'},
+	's|schemes=s'          => \$opts{'s'},
+	't|time=i'             => \$opts{'t'},
+	'w|word_size=i'        => \$opts{'w'},
+	'x|min=i'              => \$opts{'x'},
+	'y|max=i'              => \$opts{'y'},
+	'0|missing'            => \$opts{'0'},
+	'h|help'               => \$opts{'h'},
+	'n|new_only'           => \$opts{'n'},
+	'o|order'              => \$opts{'o'},
+	'q|quiet'              => \$opts{'q'},
+	'r|random'             => \$opts{'r'},
+	'T|already_tagged'     => \$opts{'T'}
+) or die("Error in command line arguments\n");
 
-if ($opts{'h'}){
+if ( $opts{'h'} ) {
 	show_help();
 	exit;
 }
-
-if (!$opts{'d'}){
+if ( !$opts{'d'} ) {
 	print "\nUsage: autotag.pl -d <database configuration>\n\n";
 	print "Help: autotag.pl -h\n";
 	exit;
 }
-
 BIGSdb::Offline::AutoTag->new(
 	{
 		config_dir       => CONFIG_DIR,
@@ -59,8 +78,8 @@ BIGSdb::Offline::AutoTag->new(
 		port             => PORT,
 		user             => USER,
 		password         => PASSWORD,
-		options			 => \%opts,
-		instance		 => $opts{'d'},
+		options          => \%opts,
+		instance         => $opts{'d'},
 	}
 );
 
@@ -71,32 +90,69 @@ Usage autotag.pl -d <database configuration>
 
 Options
 -------
--0         Marks missing loci as provisional allele 0.  Sets default word
-           size to 15.
--d <name>  Database configuration name.
--h         This help page.
--i <list>  Isolates - comma-separated list of isolate ids to scan (ignored if
-           -p used).
--I <list>  Exclude isolates - comma-separated list of isolate ids to ignore.
--l <list>  Loci - comma-separated list of loci to scan (ignored if -s used).
--m <size>  Minimum size of seqbin (bp) - limit search to isolates with at 
-           least this much sequence.
--n         New (previously untagged) isolates only
--o         Order so that isolates last tagged the longest time ago get
-           scanned first (ignored if -r used).
--p <list>  Projects - comma-separated list of project isolates to scan.
--P <list>  Exclude projects - comma-separated list of projects whose isolates
-           will be excluded.
--q         Quiet - only error messages displayed.
--r         Random - shuffle order of isolate ids to scan
--R <regex> Regex for locus names
--s <list>  Schemes - comma-separated list of scheme loci to scan.
--t <mins>  Time limit - Stop after t minutes.
--T         Scan even when sequence tagged (no designation)
--w <size>  BLASTN word size
--x <id>    Minimum isolate id
--y <id>    Maximum isolate id
+-0                        Marks missing loci as provisional allele 0. Sets
+--missing                 default word size to 15.
+           
+-d <name>                 Database configuration name.
+--database	
+
+-h                        This help page.
+--help
+
+-i <list>                 Comma-separated list of isolate ids to scan (ignored
+--isolates <list>         if -p used).
+           
+-I <list>                 Comma-separated list of isolate ids to ignore.
+--exclude_isolates <list>
+
+-l <list>                 Comma-separated list of loci to scan (ignored if -s
+--loci <list>             used).
+
+-L <list>                 Comma-separated list of loci to exclude
+--exclude_loci <list>
+
+-m <size>                 Minimum size of seqbin (bp) - limit search to
+--min_size <size>         isolates with at least this much sequence.
+           
+-n                        New (previously untagged) isolates only
+--new_only
+
+-o                        Order so that isolates last tagged the longest time
+--order                   ago get scanned first (ignored if -r used).
+           
+-p <list>                 Comma-separated list of project isolates to scan.
+--projects <list>
+
+-P <list>                 Comma-separated list of projects whose isolates will
+--exclude_projects        be excluded.
+           
+-q                        Only error messages displayed.
+--quiet
+
+-r                        Shuffle order of isolate ids to scan.
+--random
+
+-R <regex>                Regex for locus names
+--locus_regex <regex>
+
+-s <list>                 Comma-separated list of scheme loci to scan.
+--schemes <list>
+
+-t <mins>                 Stop after t minutes.
+--time <mins>
+
+-T                        Scan even when sequence tagged (no designation).
+--already_tagged
+
+-w <size>                 BLASTN word size.
+--word_size <size>
+
+-x <id>                   Minimum isolate id.
+--min <id>
+
+-y <id>                   Maximum isolate id.
+--max <id>
+
 HELP
 	return;
 }
-
--- scripts/automation/scannew.pl
+++ scripts/automation/scannew.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -T
 #Scan genomes for new alleles
 #Written by Keith Jolley
-#Copyright (c) 2013, University of Oxford
+#Copyright (c) 2013-2014, University of Oxford
 #E-mail: keith.jolley@zoo.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -33,10 +33,34 @@ use constant {
 };
 #######End Local configuration################################
 use lib (LIB_DIR);
-use Getopt::Std;
+use Getopt::Long qw(:config no_ignore_case);
 use BIGSdb::Offline::ScanNew;
 my %opts;
-getopts( 'd:A:B:i:I:l:L:m:p:P:R:s:t:w:x:y:achnorT', \%opts );
+GetOptions(
+	'A|alignment=i'        => \$opts{'A'},
+	'B|identity=i'         => \$opts{'B'},
+	'd|database=s'         => \$opts{'d'},
+	'i|isolates=s'         => \$opts{'i'},
+	'I|exclude_isolates=s' => \$opts{'I'},
+	'l|loci=s'             => \$opts{'l'},
+	'L|exclude_loci=s'     => \$opts{'L'},
+	'm|min_size=i'         => \$opts{'m'},
+	'p|projects=s'         => \$opts{'p'},
+	'P|exclude_projects=s' => \$opts{'P'},
+	'R|locus_regex=s'      => \$opts{'R'},
+	's|schemes=s'          => \$opts{'s'},
+	't|time=i'             => \$opts{'t'},
+	'w|word_size=i'        => \$opts{'w'},
+	'x|min=i'              => \$opts{'x'},
+	'y|max=i'              => \$opts{'y'},
+	'a|assign'             => \$opts{'a'},
+	'c|coding_sequences'   => \$opts{'c'},
+	'h|help'               => \$opts{'h'},
+	'n|new_only'           => \$opts{'n'},
+	'o|order'              => \$opts{'o'},
+	'r|random'             => \$opts{'r'},
+	'T|already_tagged'     => \$opts{'T'}
+) or die("Error in command line arguments\n");
 
 if ( $opts{'h'} ) {
 	show_help();
@@ -68,33 +92,75 @@ Usage scannew.pl -d <database configuration>
 
 Options
 -------
--d <name>  Database configuration name.
--a         Assign new alleles in definitions database
--A <int>   Percentage alignment (default: 100)
--B <int>   Percentage identity (default: 99)
--c         Only return complete coding sequences
--h         This help page.
--i <list>  Isolates - comma-separated list of isolate ids to scan (ignored if
-           -p used).
--I <list>  Exclude isolates - comma-separated list of isolate ids to ignore.
--l <list>  Loci - comma-separated list of loci to scan (ignored if -s used).
--L <list>  Loci to exclude - comma-separated list of loci to exclude.
--m <size>  Minimum size of seqbin (bp) - limit search to isolates with at 
-           least this much sequence.
--n         New (previously untagged) isolates only
--o         Order so that isolates last tagged the longest time ago get
-           scanned first (ignored if -r used).
--p <list>  Projects - comma-separated list of project isolates to scan.
--P <list>  Exclude projects - comma-separated list of projects whose isolates
-           will be excluded.
--r         Random - shuffle order of isolate ids to scan
--R <regex> Regex for locus names
--s <list>  Schemes - comma-separated list of scheme loci to scan.
--t <mins>  Time limit - Stop after t minutes.
--T         Scan even when sequence tagged (no designation)
--w <size>  BLASTN word size
--x <id>    Minimum isolate id
--y <id>    Maximum isolate id
+-a                        Assign new alleles in definitions database.
+--assign
+
+-A <int>                  Percentage alignment (default: 100).
+--alignment
+
+-B <int>                  Percentage identity (default: 99).
+--identity
+
+-c                        Only return complete coding sequences.
+--coding_sequences
+
+-d <name>                 Database configuration name.
+--database	
+
+-h                        This help page.
+--help
+
+-i <list>                 Comma-separated list of isolate ids to scan (ignored
+--isolates <list>         if -p used).
+           
+-I <list>                 Comma-separated list of isolate ids to ignore.
+--exclude_isolates <list>
+
+-l <list>                 Comma-separated list of loci to scan (ignored if -s
+--loci <list>             used).
+
+-L <list>                 Comma-separated list of loci to exclude
+--exclude_loci <list>
+
+-m <size>                 Minimum size of seqbin (bp) - limit search to
+--min_size <size>         isolates with at least this much sequence.
+           
+-n                        New (previously untagged) isolates only
+--new_only
+
+-o                        Order so that isolates last tagged the longest time
+--order                   ago get scanned first (ignored if -r used).
+           
+-p <list>                 Comma-separated list of project isolates to scan.
+--projects <list>
+
+-P <list>                 Comma-separated list of projects whose isolates will
+--exclude_projects        be excluded.
+           
+-r                        Shuffle order of isolate ids to scan.
+--random
+
+-R <regex>                Regex for locus names
+--locus_regex <regex>
+
+-s <list>                 Comma-separated list of scheme loci to scan.
+--schemes <list>
+
+-t <mins>                 Stop after t minutes.
+--time <mins>
+
+-T                        Scan even when sequence tagged (no designation).
+--already_tagged
+
+-w <size>                 BLASTN word size.
+--word_size <size>
+
+-x <id>                   Minimum isolate id.
+--min <id>
+
+-y <id>                   Maximum isolate id.
+--max <id>
+
 HELP
 	return;
 }
