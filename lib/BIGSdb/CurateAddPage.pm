247f778ae3c95c52b5529b3b416456aa7e3afb60
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2014-05-15 15:59:12 +0100

    Remove debug code.

--- lib/BIGSdb/CurateAddPage.pm
+++ lib/BIGSdb/CurateAddPage.pm
@@ -217,17 +217,8 @@ sub _insert {
 			}
 			push @values, $newdata->{ $_->{'name'} };
 		}
-		
-		
 		local $" = ',';
 		my $qry = "INSERT INTO $table (@table_fields) VALUES (@placeholders)";
-		
-		$logger->error($qry);
-		foreach my $value (@values){
-			$logger->error($value);
-		}
-		
-		
 		if ( $table eq 'users' ) {
 			push @$extra_inserts,
 			  {
