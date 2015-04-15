09aeb8403118857648762e3fa66dddc6e17a5a5f
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-04-15 11:22:53 +0100

    Seqbin breakdown: Option to calculate mean %GC.

--- lib/BIGSdb/Plugins/SeqbinBreakdown.pm
+++ lib/BIGSdb/Plugins/SeqbinBreakdown.pm
@@ -1,6 +1,6 @@
 #SeqbinBreakdown.pm - SeqbinBreakdown plugin for BIGSdb
 #Written by Keith Jolley
-#Copyright (c) 2010-2014, University of Oxford
+#Copyright (c) 2010-2015, University of Oxford
 #E-mail: keith.jolley@zoo.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -28,7 +28,6 @@ my $logger = get_logger('BIGSdb.Plugins');
 use Error qw(:try);
 use BIGSdb::Page qw(SEQ_METHODS);
 use List::MoreUtils qw(any uniq);
-
 use constant MAX_INSTANT_RUN => 100;
 use constant MAX_HTML_OUTPUT => 2000;
 
@@ -45,7 +44,7 @@ sub get_attributes {
 		menutext    => 'Sequence bin',
 		module      => 'SeqbinBreakdown',
 		url         => "$self->{'config'}->{'doclink'}/data_analysis.html#sequence-bin-breakdown",
-		version     => '1.1.1',
+		version     => '1.2.0',
 		dbtype      => 'isolates',
 		section     => 'breakdown,postquery',
 		input       => 'query',
@@ -149,16 +148,20 @@ sub run_job {
 		  ->update_job_status( $job_id, { message_html => "HTML output disabled as more than " . MAX_HTML_OUTPUT . " records selected." } );
 	}
 	foreach my $id (@$isolate_ids) {
-		my $contig_info = $self->_get_isolate_contig_data($id);
+		my $contig_info = $self->_get_isolate_contig_data( $id, { gc => $params->{'gc'} } );
 		$row++;
 		next if !$contig_info->{'contigs'};
 		$row_with_data++;
-		$html_buffer .= $self->_get_html_table_row( $id, $contig_info, $td ) . "\n";
-		$text_buffer .= $self->_get_text_table_row( $id, $contig_info ) . "\n";
+		$html_buffer .= $self->_get_html_table_row( $id, $contig_info, $td, { gc => $params->{'gc'} } ) . "\n";
+		$text_buffer .= $self->_get_text_table_row( $id, $contig_info, { gc => $params->{'gc'} } ) . "\n";
 		$td = $td == 1 ? 2 : 1;
 		$self->_update_totals( $data, $contig_info );
-		my $html_message = qq(<div class="scrollable">) . $self->_get_html_table_header . $html_buffer . "</tbody></table></div>";
-		my $complete     = int( $row * 100 / @$isolate_ids );
+		my $html_message =
+		    qq(<div class="scrollable">)
+		  . $self->_get_html_table_header( { gc => $params->{'gc'} } )
+		  . $html_buffer
+		  . "</tbody></table></div>";
+		my $complete = int( $row * 100 / @$isolate_ids );
 
 		if ( $row % 20 == 0 || $row == @$isolate_ids ) {
 			if ($disabled_html) {
@@ -181,7 +184,7 @@ sub run_job {
 	} else {
 		my $job_file = "$self->{'config'}->{'tmp_dir'}/$job_id\.txt";
 		open( my $job_fh, '>', $job_file ) || $logger->error("Can't open $job_file for writing");
-		say $job_fh $self->_get_text_table_header;
+		say $job_fh $self->_get_text_table_header( { gc => $params->{'gc'} } );
 		print $job_fh $text_buffer;
 		close $job_fh;
 		$self->{'jobManager'}
@@ -249,11 +252,21 @@ sub _print_interface {
 	say qq(<div class="scrollable">);
 	say $q->start_form;
 	$self->print_seqbin_isolate_fieldset( { selected_ids => $selected_ids, isolate_paste_list => 1 } );
+	$self->_print_options_fieldset;
 	$self->print_sequence_filter_fieldset;
 	$self->print_action_fieldset( { name => 'SeqbinBreakdown' } );
 	say $q->hidden($_) foreach qw (page name db set_id);
 	say $q->end_form;
 	say "</div></div>";
+	return;
+}
+
+sub _print_options_fieldset {
+	my ($self) = @_;
+	my $q = $self->{'cgi'};
+	say qq(<fieldset style="float:left"><legend>Options</legend>);
+	say $q->checkbox( -name => 'gc', -label => 'Calculate %GC' );
+	say qq(</fieldset>);
 	return;
 }
 
@@ -272,15 +285,15 @@ sub _print_table {
 	open( my $fh, '>', $text_file ) or $logger->error("Can't open temp file $text_file for writing");
 
 	foreach my $id (@$ids) {
-		my $contig_info = $self->_get_isolate_contig_data($id);
+		my $contig_info = $self->_get_isolate_contig_data( $id, { gc => $params->{'gc'} } );
 		next if !$contig_info->{'contigs'};
 		if ( !$header_displayed ) {
-			say $fh $self->_get_text_table_header;
-			say $self->_get_html_table_header;
+			say $fh $self->_get_text_table_header( { gc => $params->{'gc'} } );
+			say $self->_get_html_table_header( { gc => $params->{'gc'} } );
 			$header_displayed = 1;
 		}
-		say $self->_get_html_table_row( $id, $contig_info, $td );
-		say $fh $self->_get_text_table_row( $id, $contig_info );
+		say $self->_get_html_table_row( $id, $contig_info, $td, { gc => $params->{'gc'} } );
+		say $fh $self->_get_text_table_row( $id, $contig_info, { gc => $params->{'gc'} } );
 		$td = $td == 1 ? 2 : 1;
 		$self->_update_totals( $data, $contig_info );
 		if ( $ENV{'MOD_PERL'} ) {
@@ -317,13 +330,15 @@ sub _update_totals {
 }
 
 sub _get_html_table_header {
-	my ($self)     = @_;
+	my ( $self, $options ) = @_;
+	$options = {} if ref $options ne 'HASH';
 	my $labelfield = ucfirst( $self->{'system'}->{'labelfield'} );
+	my $gc         = $options->{'gc'} ? '<th>Mean %GC</th>' : '';
 	my $buffer     = << "HTML";
 <table class="tablesorter" id="sortTable">
 <thead>
 <tr><th>Isolate id</th><th>$labelfield</th><th>Contigs</th><th>Total length</th><th>Min</th><th>Max</th><th>Mean</th><th>&sigma;</th>
-<th>N50</th><th>N90</th><th>N95</th><th>Alleles designated</th><th>% Alleles designated</th><th>Loci tagged</th><th>% Loci tagged</th>
+<th>N50</th><th>N90</th><th>N95</th>$gc<th>Alleles designated</th><th>% Alleles designated</th><th>Loci tagged</th><th>% Loci tagged</th>
 <th>Sequence bin</th></tr>
 </thead>
 <tbody>
@@ -332,16 +347,20 @@ HTML
 }
 
 sub _get_html_table_row {
-	my ( $self, $isolate_id, $contig_info, $td ) = @_;
-	my ( $isolate_name, $contigs, $sum, $min, $max, $mean, $stddev, $single_isolate_lengths, $allele_designations, $percent_alleles,
+	my ( $self, $isolate_id, $contig_info, $td, $options ) = @_;
+	$options = {} if ref $options ne 'HASH';
+	my ( $isolate_name, $contigs, $sum, $min, $max, $mean, $stddev, $single_isolate_lengths, $gc, $allele_designations, $percent_alleles,
 		$tagged, $percent_tagged, $n_stats )
 	  = @{$contig_info}
-	  {qw(isolate_name contigs sum min max mean stddev lengths allele_designations percent_alleles tagged percent_tagged n_stats)};
+	  {qw(isolate_name contigs sum min max mean stddev lengths gc allele_designations percent_alleles tagged percent_tagged n_stats)};
+	my $q      = $self->{'cgi'};
 	my $buffer = "<tr class=\"td$td\"><td>$isolate_id</td><td>$isolate_name</td><td>$contigs</td><td>$sum</td><td>$min</td>"
 	  . "<td>$max</td><td>$mean</td>";
 	$buffer .= defined $stddev ? "<td>$stddev</td>" : '<td></td>';
+	$buffer .= "<td>$n_stats->{'N50'}</td><td>$n_stats->{'N90'}</td><td>$n_stats->{'N95'}</td>";
+	$buffer .= "<td>$gc</td>" if $options->{'gc'};
 	$buffer .=
-	    "<td>$n_stats->{'N50'}</td><td>$n_stats->{'N90'}</td><td>$n_stats->{'N95'}</td><td>$allele_designations</td>"
+	    "<td>$allele_designations</td>"
 	  . "<td>$percent_alleles</td><td>$tagged</td><td>$percent_tagged</td><td>"
 	  . "<a href=\"$self->{'system'}->{'script_name'}?page=seqbin&amp;db=$self->{'instance'}&amp;isolate_id=$isolate_id\" "
 	  . "class=\"extract_tooltip\" target=\"_blank\">Display &rarr;</a></td></tr>";
@@ -349,27 +368,33 @@ sub _get_html_table_row {
 }
 
 sub _get_text_table_header {
-	my ($self) = @_;
+	my ( $self, $options ) = @_;
+	$options = {} if ref $options ne 'HASH';
 	my $labelfield = ucfirst( $self->{'system'}->{'labelfield'} );
-	return "Isolate id\t$labelfield\tContigs\tTotal length\tMin\tMax\tMean\tStdDev\tN50\tN90\tN95\tAlleles designated\t"
+	my $gc = $options->{'gc'} ? "%GC\t" : '';
+	return "Isolate id\t$labelfield\tContigs\tTotal length\tMin\tMax\tMean\tStdDev\tN50\tN90\tN95\t${gc}Alleles designated\t"
 	  . "%Alleles designated\tLoci tagged\t%Loci tagged";
 }
 
 sub _get_text_table_row {
-	my ( $self, $isolate_id, $contig_info ) = @_;
-	my ( $isolate_name, $contigs, $sum, $min, $max, $mean, $stddev, $single_isolate_lengths, $allele_designations, $percent_alleles,
+	my ( $self, $isolate_id, $contig_info, $options ) = @_;
+	$options = {} if ref $options ne 'HASH';
+	my ( $isolate_name, $contigs, $sum, $min, $max, $mean, $stddev, $single_isolate_lengths, $gc, $allele_designations, $percent_alleles,
 		$tagged, $percent_tagged, $n_stats )
 	  = @{$contig_info}
-	  {qw(isolate_name contigs sum min max mean stddev lengths allele_designations percent_alleles tagged percent_tagged n_stats)};
+	  {qw(isolate_name contigs sum min max mean stddev lengths gc allele_designations percent_alleles tagged percent_tagged n_stats)};
 	my $buffer = "$isolate_id\t$isolate_name\t$contigs\t$sum\t$min\t$max\t$mean\t";
 	$buffer .= "$stddev" if defined $stddev;
-	$buffer .=
-	  "\t$n_stats->{'N50'}\t$n_stats->{'N90'}\t$n_stats->{'N95'}\t$allele_designations\t$percent_alleles\t$tagged\t" . "$percent_tagged";
+	$buffer .= "\t$n_stats->{'N50'}\t$n_stats->{'N90'}\t$n_stats->{'N95'}\t";
+	$buffer .= "$gc\t"   if $options->{'gc'};
+	$buffer .= "$allele_designations\t$percent_alleles\t$tagged\t" . "$percent_tagged";
 	return $buffer;
 }
 
 sub _get_isolate_contig_data {
-	my ( $self, $isolate_id ) = @_;
+	my ( $self, $isolate_id, $options ) = @_;
+	$options = {} if ref $options ne 'HASH';
+	my $q = $self->{'cgi'};
 	eval { $self->{'sql'}->{'contig_info'}->execute( $isolate_id, @{ $self->{'criteria'} } ) };
 	$logger->error($@) if $@;
 	my $set_id = $self->get_set_id;
@@ -388,6 +413,11 @@ sub _get_isolate_contig_data {
 		$data->{'tagged'}         = $tagged;
 		$data->{'percent_tagged'} = BIGSdb::Utils::decimal_place( 100 * ( $tagged / @{ $self->{'cache'}->{'loci'} } ), 1 );
 		$data->{'n_stats'}        = BIGSdb::Utils::get_N_stats( $data->{'sum'}, $data->{'lengths'} );
+	}
+	if ( $options->{'gc'} ) {
+		eval { $self->{'sql'}->{'gc'}->execute( $isolate_id, @{ $self->{'criteria'} } ) };
+		$logger->error($@) if $@;
+		( $data->{'gc'} ) = BIGSdb::Utils::decimal_place( ( $self->{'sql'}->{'gc'}->fetchrow_array ) * 100, 1 );
 	}
 	return $data;
 }
@@ -439,6 +469,9 @@ sub _initiate_statement_handles {
 	$self->{'sql'}->{'contig_lengths'} =
 	  $self->{'db'}->prepare( "SELECT length(sequence) FROM sequence_bin LEFT JOIN experiment_sequences "
 		  . "ON sequence_bin.id=seqbin_id WHERE isolate_id=?$exclusion_clause ORDER BY length(sequence) DESC" );
+	$self->{'sql'}->{'gc'} =
+	  $self->{'db'}->prepare( "select SUM(CAST(length(regexp_replace(sequence,'[^GCgc]+','','g')) AS float))/"
+		  . "SUM(length(sequence)) AS gc FROM sequence_bin WHERE isolate_id=?$exclusion_clause GROUP BY isolate_id " );
 	my $set_id = $self->get_set_id;
 	my $set_clause =
 	  $set_id
