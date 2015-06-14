1f9bd43df3be71e89ccf26b7d33ef2858134e92f
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-06-14 21:31:23 +0100

    Reformat AlleleSequencePage.pm.

--- lib/BIGSdb/AlleleSequencePage.pm
+++ lib/BIGSdb/AlleleSequencePage.pm
@@ -54,43 +54,45 @@ sub print_content {
 	my $locus      = $q->param('locus');
 	my $locus_info = $self->{'datastore'}->get_locus_info($locus);
 	if ( !BIGSdb::Utils::is_int($isolate_id) ) {
-		say qq(<div class="box" id="statusbad"><p>Isolate id must be an integer.</p></div>);
+		say q(<div class="box" id="statusbad"><p>Isolate id must be an integer.</p></div>);
 		return;
 	}
-	my $exists = $self->{'datastore'}->run_query( "SELECT EXISTS(SELECT * FROM $self->{'system'}->{'view'} WHERE id=?)", $isolate_id );
+	my $exists =
+	  $self->{'datastore'}
+	  ->run_query( "SELECT EXISTS(SELECT * FROM $self->{'system'}->{'view'} WHERE id=?)", $isolate_id );
 	if ( !$exists ) {
-		say qq(<div class="box" id="statusbad"><p>The database contains no record of this isolate.</p></div>);
+		say q(<div class="box" id="statusbad"><p>The database contains no record of this isolate.</p></div>);
 		return;
 	}
 	if ( !$self->{'datastore'}->is_locus($locus) ) {
-		say qq(<div class="box" id="statusbad"><p>Invalid locus selected.</p></div>);
+		say q(<div class="box" id="statusbad"><p>Invalid locus selected.</p></div>);
 		return;
 	}
 	my @name          = $self->get_name($isolate_id);
 	my $display_locus = $self->clean_locus($locus);
-	print "<h1>$display_locus allele sequence: id-$isolate_id";
-	print " (@name)" if $name[1];
-	say "</h1>";
+	print qq(<h1>$display_locus allele sequence: id-$isolate_id);
+	print qq( (@name)) if $name[1];
+	say q(</h1>);
 	my $flanking = $self->{'prefs'}->{'flanking'};
-	my $qry = "SELECT allele_sequences.id,seqbin_id,start_pos,end_pos,reverse,complete,method,length(sequence) AS seqlength FROM "
-	  . "allele_sequences LEFT JOIN sequence_bin ON allele_sequences.seqbin_id = sequence_bin.id WHERE sequence_bin.isolate_id=? "
-	  . "AND locus=? ORDER BY complete desc,allele_sequences.datestamp";
-	my $data = $self->{'datastore'}->run_query($qry,[$isolate_id, $locus],{fetch=>'all_arrayref'});
+	my $qry =
+	    'SELECT allele_sequences.id,seqbin_id,start_pos,end_pos,reverse,complete,method,length(sequence) '
+	  . 'AS seqlength FROM allele_sequences LEFT JOIN sequence_bin ON allele_sequences.seqbin_id = sequence_bin.id '
+	  . 'WHERE sequence_bin.isolate_id=? AND locus=? ORDER BY complete desc,allele_sequences.datestamp';
+	my $data = $self->{'datastore'}->run_query( $qry, [ $isolate_id, $locus ], { fetch => 'all_arrayref' } );
 	my $buffer;
-	
 
 	foreach my $allele_sequence (@$data) {
-		my ( $id, $seqbin_id, $start_pos, $end_pos, $reverse, $complete, $method, $seqlength )  = @$allele_sequence;
+		my ( $id, $seqbin_id, $start_pos, $end_pos, $reverse, $complete, $method, $seqlength ) = @$allele_sequence;
 		my $update_buffer = '';
-				if ( $self->{'curate'} ) {
-			$update_buffer = qq( <a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=tagUpdate&amp;id=)
+		if ( $self->{'curate'} ) {
+			$update_buffer =
+			    qq( <a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=tagUpdate&amp;id=)
 			  . qq($id" class="smallbutton">Update</a>\n);
 		}
-		$buffer .= "<h2>Contig position$update_buffer</h2>\n";
-		$buffer .= qq(<dl class="data">);
+		$buffer .= qq(<h2>Contig position$update_buffer</h2>\n);
+		$buffer .= q(<dl class="data">);
 		$buffer .= qq(<dt class="dontend">sequence bin id</dt><dd>$seqbin_id</dd>);
 		$buffer .= qq(<dt class="dontend">contig length</dt><dd>$seqlength</dd>);
-
 		my $translate = ( $locus_info->{'coding_sequence'} || $locus_info->{'data_type'} eq 'peptide' ) ? 1 : 0;
 		my $orf = $locus_info->{'orf'} || 1;
 		$buffer .= qq(<dt class="dontend">start</dt><dd>$start_pos</dd>\n);
@@ -99,34 +101,43 @@ sub print_content {
 		$buffer .= qq(<dt class="dontend">length</dt><dd>$length</dd>\n);
 		my $orientation = $reverse ? 'reverse' : 'forward';
 		$buffer .= qq(<dt class="dontend">orientation</dt><dd>$orientation</dd>\n);
-		$buffer .= qq(<dt class="dontend">complete</dt><dd>) . ( $complete ? 'yes' : 'no' ) . qq(</dd>\n);
+		$buffer .= q(<dt class="dontend">complete</dt><dd>) . ( $complete ? 'yes' : 'no' ) . qq(</dd>\n);
 		$buffer .= qq(<dt class="dontend">method</dt><dd>$method</dd>\n);
 		my $display = $self->format_seqbin_sequence(
-			{ seqbin_id => $seqbin_id, reverse => $reverse, start => $start_pos, end => $end_pos, translate => $translate, orf => $orf } );
-		$buffer .= "</dl>";
-		$buffer .= "<h2>Sequence</h2>\n";
+			{
+				seqbin_id => $seqbin_id,
+				reverse   => $reverse,
+				start     => $start_pos,
+				end       => $end_pos,
+				translate => $translate,
+				orf       => $orf
+			}
+		);
+		$buffer .= q(</dl>);
+		$buffer .= qq(<h2>Sequence</h2>\n);
 		$buffer .= qq(<div class="seq">$display->{'seq'}</div>\n);
 
 		if ($translate) {
-			$buffer .= "<h2>Translation</h2>\n";
+			$buffer .= qq(<h2>Translation</h2>\n);
 			my @stops = @{ $display->{'internal_stop'} };
 			if (@stops) {
 				my $plural = @stops == 1 ? '' : 's';
 				local $" = ', ';
-				$buffer .= qq[<span class="highlight">Internal stop codon$plural at position$plural: @stops (numbering includes upstream ]
-				  . qq[flanking sequence).</span>\n];
+				$buffer .= qq[<span class="highlight">Internal stop codon$plural at position$plural: @stops ]
+				  . qq[(numbering includes upstream flanking sequence).</span>\n];
 			}
 			$buffer .= qq(<pre class="sixpack">\n);
 			$buffer .= $display->{'sixpack'};
-			$buffer .= "</pre>\n";
+			$buffer .= qq(</pre>\n);
 		}
 	}
 	if ($buffer) {
-		say qq(<div class="box" id="resultspanel">\n<div class="scrollable">);
+		say q(<div class="box" id="resultspanel"><div class="scrollable">);
 		say $buffer;
-		say "</div></div>";
+		say q(</div></div>);
 	} else {
-		say qq(<div class="box" id="statusbad"><p>This isolate does not have a sequence defined for locus $display_locus.</p></div>);
+		say q(<div class="box" id="statusbad"><p>This isolate does not have a sequence defined for )
+		  . qq(locus $display_locus.</p></div>);
 	}
 	return;
 }
@@ -135,14 +146,13 @@ sub get_title {
 	my ($self)     = @_;
 	my $isolate_id = $self->{'cgi'}->param('id');
 	my $locus      = $self->{'cgi'}->param('locus');
-	return "Invalid isolate id" if !BIGSdb::Utils::is_int($isolate_id);
-	return "Invalid locus" if !$self->{'datastore'}->is_locus($locus);
+	return 'Invalid isolate id' if !BIGSdb::Utils::is_int($isolate_id);
+	return 'Invalid locus' if !$self->{'datastore'}->is_locus($locus);
 	my @name = $self->get_name($isolate_id);
 	( my $display_locus = $locus ) =~ tr/_/ /;
 	my $title = "$display_locus allele sequence: id-$isolate_id";
 	$title .= " (@name)" if $name[1];
-	$title .= ' - ';
-	$title .= "$self->{'system'}->{'description'}";
+	$title .= " - $self->{'system'}->{'description'}";
 	return $title;
 }
 1;
