b4162f02eb464596240cceca5c4cc81b3145669c
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-06-29 13:07:54 +0100

    CAST to integer when performing allele_designation query with integers.

--- lib/BIGSdb/TableQueryPage.pm
+++ lib/BIGSdb/TableQueryPage.pm
@@ -552,7 +552,7 @@ sub _run_query {
 							$qry .= ( $text eq 'null' ? "$table.$field is null" : "$table.$field = '$text'" );
 						}
 					} else {
-						if ( $table eq 'sequences' && $field eq 'allele_id' ) {
+						if ( ($table eq 'sequences' || $table eq 'allele_designations') && $field eq 'allele_id' ) {
 							if ( $self->_are_only_int_allele_ids_used && BIGSdb::Utils::is_int($text) ) {
 								$qry .= "CAST($table.$field AS integer)";
 							} else {
