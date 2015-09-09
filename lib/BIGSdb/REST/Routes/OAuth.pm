kjolley/BIGSdb

commit 86c08896675e8c34e868ebe4e23b5a3ac89d03d1

Reformat/refactor REST modules.

lib/BIGSdb/REST/Routes/OAuth.pm
@@ -33,12 +33,12 @@ any [qw(get post)] => '/db/:db/oauth/get_request_token' => sub {
 	my $params          = params;
 	my $db              = param('db');
 	my $consumer_secret = $self->{'datastore'}->run_query(
-		"SELECT client_secret FROM clients WHERE client_id=?",
+		'SELECT client_secret FROM clients WHERE client_id=?',
 		param('oauth_consumer_key'),
 		{ db => $self->{'auth_db'}, cache => 'REST::get_client_secret' }
 	);
 	if ( !$consumer_secret ) {
-		send_error( "Unrecognized client", 403 );
+		send_error( 'Unrecognized client', 403 );
 	}
 	my $request_params = {};
 	$request_params->{$_} = param($_) foreach qw(
@@ -60,35 +60,34 @@ any [qw(get post)] => '/db/:db/oauth/get_request_token' => sub {
 	};
 
 	if ($@) {
-		warn $@;
-		if ( $@ =~ /Missing required parameter \'(\w+?)\'/ ) {
+		if ( $@ =~ /Missing\ required\ parameter\ \'(\w+?)\'/x ) {
 			send_error( "Invalid token request. Missing required parameter: $1", 400 );
 		} else {
 			$self->{'logger'}->error($@);
-			send_error( "Invalid token request", 400 );
+			send_error( 'Invalid token request', 400 );
 		}
 	}
-	$self->{'logger'}->debug( "Request string: " . $request->signature_base_string );
+	$self->{'logger'}->debug( 'Request string: ' . $request->signature_base_string );
 	if ( !$request->verify ) {
-		send_error( "Signature verification failed", 401 );
+		send_error( 'Signature verification failed', 401 );
 	}
 	if ( abs( $request->timestamp - time ) > REQUEST_TOKEN_TIMEOUT ) {
-		send_error( "Request timestamp more than " . REQUEST_TOKEN_TIMEOUT . " seconds from current time.", 401 );
+		send_error( 'Request timestamp more than ' . REQUEST_TOKEN_TIMEOUT . ' seconds from current time.', 401 );
 	}
 	my $request_repeated = $self->{'datastore'}->run_query(
-		"SELECT EXISTS(SELECT * FROM request_tokens WHERE (nonce,timestamp)=(?,?))",
+		'SELECT EXISTS(SELECT * FROM request_tokens WHERE (nonce,timestamp)=(?,?))',
 		[ param('oauth_nonce'), param('oauth_timestamp') ],
 		{ db => $self->{'auth_db'} }
 	);
 	if ($request_repeated) {
-		send_error( "Request with same nonce and timestamp already made", 401 );
+		send_error( 'Request with same nonce and timestamp already made', 401 );
 	}
 	my $token        = BIGSdb::Utils::random_string(32);
 	my $token_secret = BIGSdb::Utils::random_string(32);
 	eval {
-		$self->{'auth_db'}->do( "DELETE FROM request_tokens WHERE start_time<?", undef, time - REQUEST_TOKEN_EXPIRES );
+		$self->{'auth_db'}->do( 'DELETE FROM request_tokens WHERE start_time<?', undef, time - REQUEST_TOKEN_EXPIRES );
 		$self->{'auth_db'}->do(
-			"INSERT INTO request_tokens (token,secret,client_id,nonce,timestamp,start_time) VALUES (?,?,?,?,?,?)",
+			'INSERT INTO request_tokens (token,secret,client_id,nonce,timestamp,start_time) VALUES (?,?,?,?,?,?)',
 			undef, $token, $token_secret, param('oauth_consumer_key'),
 			param('oauth_nonce'), param('oauth_timestamp'), time
 		);
@@ -96,7 +95,7 @@ any [qw(get post)] => '/db/:db/oauth/get_request_token' => sub {
 	if ($@) {
 		$self->{'logger'}->error($@);
 		$self->{'auth_db'}->rollback;
-		send_error( "Error creating request token", 400 );
+		send_error( 'Error creating request token', 400 );
 	}
 	$self->{'auth_db'}->commit;
 	return { oauth_token => $token, oauth_token_secret => $token_secret, oauth_callback_confirmed => 'true' };
@@ -108,23 +107,26 @@ any [qw(get post)] => '/db/:db/oauth/get_access_token' => sub {
 	my $params          = params;
 	my $db              = param('db');
 	my $consumer_secret = $self->{'datastore'}->run_query(
-		"SELECT client_secret FROM clients WHERE client_id=?",
+		'SELECT client_secret FROM clients WHERE client_id=?',
 		param('oauth_consumer_key'),
 		{ db => $self->{'auth_db'}, cache => 'REST::get_client_secret' }
 	);
 	if ( !$consumer_secret ) {
-		send_error( "Unrecognized client", 403 );
+		send_error( 'Unrecognized client', 403 );
 	}
-	my $request_token = $self->{'datastore'}->run_query( "SELECT * FROM request_tokens WHERE token=?",
+	my $request_token = $self->{'datastore'}->run_query( 'SELECT * FROM request_tokens WHERE token=?',
 		param('oauth_token'), { fetch => 'row_hashref', db => $self->{'auth_db'} } );
 	if ( !$request_token->{'secret'} ) {
-		send_error( "Invalid request token.  Generate new request token (/get_request_token).", 401 );
-	} elsif ( !$request_token->{'verifier'} || $request_token->{'verifier'} ne param('oauth_verifier') ) {
-		send_error( "Invalid verifier code.", 401 );
-	} elsif ( $request_token->{'redeemed'} ) {
-		send_error( "Request token has already been redeemed.  Generate new request token (/get_request_token).", 401 );
-	} elsif ( abs( $request_token->{'timestamp'} - time ) > REQUEST_TOKEN_EXPIRES ) {
-		send_error( "Request token has expired.  Generate new request token (/get_request_token).", 401 );
+		send_error( 'Invalid request token.  Generate new request token (/get_request_token).', 401 );
+	}
+	if ( !$request_token->{'verifier'} || $request_token->{'verifier'} ne param('oauth_verifier') ) {
+		send_error( 'Invalid verifier code.', 401 );
+	}
+	if ( $request_token->{'redeemed'} ) {
+		send_error( 'Request token has already been redeemed.  Generate new request token (/get_request_token).', 401 );
+	}
+	if ( abs( $request_token->{'timestamp'} - time ) > REQUEST_TOKEN_EXPIRES ) {
+		send_error( 'Request token has expired.  Generate new request token (/get_request_token).', 401 );
 	}
 	my $request_params = {};
 	$request_params->{$_} = param($_) foreach qw(
@@ -148,33 +150,33 @@ any [qw(get post)] => '/db/:db/oauth/get_access_token' => sub {
 	};
 
 	if ($@) {
-		warn $@;
-		if ( $@ =~ /Missing required parameter \'(\w+?)\'/ ) {
+		if ( $@ =~ /Missing\ required\ parameter\ \'(\w+?)\'/x ) {
 			send_error( "Invalid token request. Missing required parameter: $1.", 400 );
 		} else {
 			$self->{'logger'}->error($@);
-			send_error( "Invalid token request.", 400 );
+			send_error( 'Invalid token request.', 400 );
 		}
 	}
-	$self->{'logger'}->debug( "Request string: " . $request->signature_base_string );
+	$self->{'logger'}->debug( 'Request string: ' . $request->signature_base_string );
 	if ( !$request->verify ) {
-		send_error( "Signature verification failed.", 401 );
+		send_error( 'Signature verification failed.', 401 );
 	}
 	my $access_token        = BIGSdb::Utils::random_string(32);
 	my $access_token_secret = BIGSdb::Utils::random_string(32);
 	eval {
-		$self->{'auth_db'}->do( "UPDATE request_tokens SET redeemed=? WHERE token=?", undef, 1, $request_token->{'token'} );
+		$self->{'auth_db'}
+		  ->do( 'UPDATE request_tokens SET (redeemed,token)=(?,?)', undef, 1, $request_token->{'token'} );
 
 		#Replace existing access token for same user.
 		$self->{'auth_db'}->do(
-			"DELETE FROM access_tokens WHERE (client_id,username,dbase)=(?,?,?)",
+			'DELETE FROM access_tokens WHERE (client_id,username,dbase)=(?,?,?)',
 			undef,
 			param('oauth_consumer_key'),
 			$request_token->{'username'},
 			$request_token->{'dbase'}
 		);
 		$self->{'auth_db'}->do(
-			"INSERT INTO access_tokens (token,secret,client_id,datestamp,username,dbase) VALUES (?,?,?,?,?,?)",
+			'INSERT INTO access_tokens (token,secret,client_id,datestamp,username,dbase) VALUES (?,?,?,?,?,?)',
 			undef, $access_token, $access_token_secret, param('oauth_consumer_key'),
 			'now',
 			$request_token->{'username'},
@@ -196,18 +198,24 @@ any [qw(get post)] => '/db/:db/oauth/get_session_token' => sub {
 	my $params          = params;
 	my $db              = param('db');
 	my $consumer_secret = $self->{'datastore'}->run_query(
-		"SELECT client_secret FROM clients WHERE client_id=?",
+		'SELECT client_secret FROM clients WHERE client_id=?',
 		param('oauth_consumer_key'),
 		{ db => $self->{'auth_db'}, cache => 'REST::get_client_secret' }
 	);
 	if ( !$consumer_secret ) {
-		send_error( "Unrecognized client", 403 );
+		send_error( 'Unrecognized client', 403 );
 	}
-	my $access_token = $self->{'datastore'}->run_query( "SELECT * FROM access_tokens WHERE token=?",
+	my $access_token = $self->{'datastore'}->run_query(
+		'SELECT * FROM access_tokens WHERE token=?',
 		param('oauth_token'),
-		{ fetch => 'row_hashref', db => $self->{'auth_db'}, cache => 'REST::Routes::OAuth::get_session_token::access_token' } );
+		{
+			fetch => 'row_hashref',
+			db    => $self->{'auth_db'},
+			cache => 'REST::Routes::OAuth::get_session_token::access_token'
+		}
+	);
 	if ( !$access_token->{'secret'} ) {
-		send_error( "Invalid access token.  Generate new request token (/get_access_token).", 401 );
+		send_error( 'Invalid access token.  Generate new request token (/get_access_token).', 401 );
 	}
 	my $request_params = {};
 	$request_params->{$_} = param($_) foreach qw(
@@ -230,39 +238,43 @@ any [qw(get post)] => '/db/:db/oauth/get_session_token' => sub {
 	};
 
 	if ($@) {
-		warn $@;
-		if ( $@ =~ /Missing required parameter \'(\w+?)\'/ ) {
+		if ( $@ =~ /Missing\ required\ parameter\ \'(\w+?)\'/x ) {
 			send_error( "Invalid token request. Missing required parameter: $1.", 400 );
 		} else {
 			$self->{'logger'}->error($@);
-			send_error( "Invalid token request.", 400 );
+			send_error( 'Invalid token request.', 400 );
 		}
 	}
-	$self->{'logger'}->debug( "Request string: " . $request->signature_base_string );
+	$self->{'logger'}->debug( 'Request string: ' . $request->signature_base_string );
 	if ( !$request->verify ) {
-		send_error( "Signature verification failed.", 401 );
+		send_error( 'Signature verification failed.', 401 );
 	}
 	my $request_repeated = $self->{'datastore'}->run_query(
-		"SELECT EXISTS(SELECT * FROM api_sessions WHERE (nonce,timestamp)=(?,?))",
+		'SELECT EXISTS(SELECT * FROM api_sessions WHERE (nonce,timestamp)=(?,?))',
 		[ param('oauth_nonce'), param('oauth_timestamp') ],
 		{ db => $self->{'auth_db'}, cache => 'REST::Routes::OAuth::get_session_token::session_exists' }
 	);
 	if ($request_repeated) {
-		send_error( "Request with same nonce and timestamp already made", 401 );
+		send_error( 'Request with same nonce and timestamp already made', 401 );
 	} elsif ( abs( $request->timestamp - time ) > ACCESS_TOKEN_TIMEOUT ) {
-		send_error( "Request timestamp more than " . ACCESS_TOKEN_TIMEOUT . " seconds from current time.", 401 );
+		send_error( 'Request timestamp more than ' . ACCESS_TOKEN_TIMEOUT . ' seconds from current time.', 401 );
 	}
 	my $session_token        = BIGSdb::Utils::random_string(32);
 	my $session_token_secret = BIGSdb::Utils::random_string(32);
 	$self->delete_old_sessions;
 	eval {
 		$self->{'auth_db'}->do(
-			"INSERT INTO api_sessions (dbase,username,client_id,session,secret,nonce,timestamp,start_time) VALUES (?,?,?,?,?,?,?,?)",
+			'INSERT INTO api_sessions (dbase,username,client_id,session,'
+			  . 'secret,nonce,timestamp,start_time) VALUES (?,?,?,?,?,?,?,?)',
 			undef,
 			$access_token->{'dbase'},
 			$access_token->{'username'},
 			param('oauth_consumer_key'),
-			$session_token, $session_token_secret, param('oauth_nonce'), param('oauth_timestamp'), time
+			$session_token,
+			$session_token_secret,
+			param('oauth_nonce'),
+			param('oauth_timestamp'),
+			time
 		);
 	};
 	if ($@) {

lib/BIGSdb/REST/Interface.pm
@@ -78,7 +78,7 @@ sub _initiate {
 hook before => sub {
 	my $self        = setting('self');
 	my $request_uri = request->uri();
-	$self->{'instance'} = $request_uri =~ /^\/db\/([\w\d\-_]+)/ ? $1 : '';
+	$self->{'instance'} = $request_uri =~ /^\/db\/([\w\d\-_]+)/x ? $1 : '';
 	my $full_path = "$self->{'dbase_config_dir'}/$self->{'instance'}/config.xml";
 	if ( !$self->{'instance'} ) {
 		undef $self->{'system'};
@@ -97,7 +97,7 @@ hook before => sub {
 		$self->{'system'} = $self->{'xmlHandler'}->get_system_hash;
 	}
 	$self->set_system_overrides;
-	$ENV{'PATH'} = '/bin:/usr/bin';              ## no critic (RequireLocalizedPunctuationVars) #so we don't foul taint check
+	$ENV{'PATH'} = '/bin:/usr/bin';    ## no critic (RequireLocalizedPunctuationVars) #so we don't foul taint check
 	delete @ENV{qw(IFS CDPATH ENV BASH_ENV)};    # Make %ENV safer
 	$self->{'system'}->{'read_access'} ||= 'public';                          #everyone can view by default
 	$self->{'system'}->{'host'}        ||= $self->{'host'} || 'localhost';
@@ -109,8 +109,9 @@ hook before => sub {
 		$self->{'system'}->{'view'}       ||= 'isolates';
 		$self->{'system'}->{'labelfield'} ||= 'isolate';
 		if ( !$self->{'xmlHandler'}->is_field( $self->{'system'}->{'labelfield'} ) ) {
-			$logger->error( "The defined labelfield '$self->{'system'}->{'labelfield'}' does not exist in the database.  "
-				  . "Please set the labelfield attribute in the system tag of the database XML file." );
+			$logger->error( "The defined labelfield '$self->{'system'}->{'labelfield'}' does not exist in the "
+				  . 'database. Please set the labelfield attribute in the system tag of the database XML file.' )
+			  ;
 		}
 	}
 	$self->{'dataConnector'}->initiate( $self->{'system'}, $self->{'config'} );
@@ -118,18 +119,24 @@ hook before => sub {
 	$self->initiate_authdb if ( $self->{'system'}->{'authentication'} // '' ) eq 'builtin';
 	$self->setup_datastore;
 	$self->_initiate_view;
-	$self->{'page_size'} = ( BIGSdb::Utils::is_int( param('page_size') ) && param('page_size') > 0 ) ? param('page_size') : PAGE_SIZE;
-	if ( ( $self->{'system'}->{'dbtype'} // '' ) eq 'isolates' || ( $self->{'system'}->{'dbtype'} // '' ) eq 'sequences' ) {
+	$self->{'page_size'} =
+	  ( BIGSdb::Utils::is_int( param('page_size') ) && param('page_size') > 0 ) ? param('page_size') : PAGE_SIZE;
+	if (   ( $self->{'system'}->{'dbtype'} // '' ) eq 'isolates'
+		|| ( $self->{'system'}->{'dbtype'} // '' ) eq 'sequences' )
+	{
 
-		if ( ( $self->{'system'}->{'read_access'} // '' ) ne 'public' && $request_uri !~ /^\/db\/$self->{'instance'}\/oauth\// ) {
+		if ( ( $self->{'system'}->{'read_access'} // '' ) ne 'public'
+			&& $request_uri !~ /^\/db\/$self->{'instance'}\/oauth\//x )
+		{
 			send_error( 'Unauthorized', 401 ) if !$self->_is_authorized;
 		}
 	}
 	return;
 };
 
-#Drop the connection because we may have hundreds of databases on the system.  Keeping them all open will exhaust resources.
-#This could be made optional for systems with only a few databases.
+#Drop the connection because we may have hundreds of databases on the system.  
+#Keeping them all open will exhaust resources. This could be made optional 
+#for systems with only a few databases.
 hook after => sub {
 	my $self = setting('self');
 	$self->{'dataConnector'}->drop_all_connections;
@@ -140,24 +147,24 @@ sub _is_authorized {
 	my $params = params;
 	my $db     = param('db');
 	if ( !param('oauth_consumer_key') ) {
-		send_error( "Unauthorized - Generate new session token.", 401 );
+		send_error( 'Unauthorized - Generate new session token.', 401 );
 	}
 	my $client = $self->{'datastore'}->run_query(
-		"SELECT * FROM clients WHERE client_id=?",
+		'SELECT * FROM clients WHERE client_id=?',
 		param('oauth_consumer_key'),
 		{ db => $self->{'auth_db'}, fetch => 'row_hashref', cache => 'REST::get_client_secret' }
 	);
 	if ( !$client->{'client_secret'} ) {
-		send_error( "Unrecognized client", 403 );
+		send_error( 'Unrecognized client', 403 );
 	}
 	$self->delete_old_sessions;
 	my $session_token = $self->{'datastore'}->run_query(
-		"SELECT * FROM api_sessions WHERE (session,dbase)=(?,?)",
+		'SELECT * FROM api_sessions WHERE (session,dbase)=(?,?)',
 		[ param('oauth_token'), $self->{'system'}->{'db'} ],
 		{ fetch => 'row_hashref', db => $self->{'auth_db'}, cache => 'REST::Interface::is_authorized::api_sessions' }
 	);
 	if ( !$session_token->{'secret'} ) {
-		send_error( "Invalid session token.  Generate new request token (/get_access_token).", 401 );
+		send_error( 'Invalid session token.  Generate new request token (/get_access_token).', 401 );
 	}
 	my $request_params = {};
 	$request_params->{$_} = param($_) foreach qw(
@@ -180,20 +187,19 @@ sub _is_authorized {
 	};
 
 	if ($@) {
-		warn $@;
-		if ( $@ =~ /Missing required parameter \'(\w+?)\'/ ) {
+		if ( $@ =~ /Missing\ required\ parameter\ \'(\w+?)\'/x ) {
 			send_error( "Invalid token request. Missing required parameter: $1.", 400 );
 		} else {
 			$self->{'logger'}->error($@);
-			send_error( "Invalid token request.", 400 );
+			send_error( 'Invalid token request.', 400 );
 		}
 	}
 	if ( !$request->verify ) {
-		$self->{'logger'}->debug( "Request string: " . $request->signature_base_string );
-		send_error( "Signature verification failed.", 401 );
+		$self->{'logger'}->debug( 'Request string: ' . $request->signature_base_string );
+		send_error( 'Signature verification failed.', 401 );
 	}
 	my $authorize = $self->{'datastore'}->run_query(
-		"SELECT authorize FROM client_permissions WHERE (client_id,dbase)=(?,?)",
+		'SELECT authorize FROM client_permissions WHERE (client_id,dbase)=(?,?)',
 		[ param('oauth_consumer_key'), $self->{'system'}->{'db'} ],
 		{ db => $self->{'auth_db'}, cache => 'REST::Interface::is_authorized::client_permissions' }
 	);
@@ -204,14 +210,14 @@ sub _is_authorized {
 		$client_authorized = ( !$authorize || $authorize eq 'deny' ) ? 0 : 1;
 	}
 	if ( !$client_authorized ) {
-		send_error( "Client is unauthorized to access this database.", 401 );
+		send_error( 'Client is unauthorized to access this database.', 401 );
 	}
 	return 1;
 }
 
 sub delete_old_sessions {
 	my ($self) = @_;
-	eval { $self->{'auth_db'}->do( "DELETE FROM api_sessions WHERE start_time<?", undef, time - SESSION_EXPIRES ) };
+	eval { $self->{'auth_db'}->do( 'DELETE FROM api_sessions WHERE start_time<?', undef, time - SESSION_EXPIRES ) };
 	if ($@) {
 		$self->{'auth_db'}->rollback;
 		$self->{'logger'}->error($@);
@@ -237,7 +243,7 @@ sub _initiate_view {
 	my $set_id = $self->get_set_id;
 	if ( defined $self->{'system'}->{'view'} && $set_id ) {
 		if ( $self->{'system'}->{'views'} && BIGSdb::Utils::is_int($set_id) ) {
-			my $view = $self->{'datastore'}->run_query( "SELECT view FROM set_view WHERE set_id=?", $set_id );
+			my $view = $self->{'datastore'}->run_query( 'SELECT view FROM set_view WHERE set_id=?', $set_id );
 			$self->{'system'}->{'view'} = $view if defined $view;
 		}
 	}
@@ -247,16 +253,17 @@ sub _initiate_view {
 #Get the contents of the rest_db database.
 sub get_resources {
 	my ($self) = @_;
-	my $groups = $self->{'datastore'}->run_query( "SELECT * FROM groups ORDER BY name",
+	my $groups = $self->{'datastore'}->run_query( 'SELECT * FROM groups ORDER BY name',
 		undef, { fetch => 'all_arrayref', slice => {}, cache => 'REST::Interface::get_resources::groups' } );
 	my $resources = [];
 	foreach my $group (@$groups) {
 		my $group_resources =
-		  $self->{'datastore'}->run_query( "SELECT dbase_config FROM group_resources WHERE group_name=? ORDER BY dbase_config",
+		  $self->{'datastore'}
+		  ->run_query( 'SELECT dbase_config FROM group_resources WHERE group_name=? ORDER BY dbase_config',
 			$group->{'name'}, { fetch => 'col_arrayref', cache => 'REST::Interface::get_resources::resources' } );
 		my @databases;
 		foreach my $dbase_config (@$group_resources) {
-			my $desc = $self->{'datastore'}->run_query( "SELECT description FROM resources WHERE dbase_config=?",
+			my $desc = $self->{'datastore'}->run_query( 'SELECT description FROM resources WHERE dbase_config=?',
 				$dbase_config, { cache => 'REST::Interface::get_resources::desc' } );
 			push @databases, { dbase_config => $dbase_config, description => $desc };
 		}
@@ -281,9 +288,9 @@ sub get_paging {
 	if ( $page != $pages ) {
 		$paging->{'last'} = request->uri_base . "$route?page=$pages&page_size=$self->{'page_size'}";
 	}
-	if (%$paging){
+	if (%$paging) {
 		$paging->{'return_all'} = request->uri_base . "$route?return_all=1";
-	} 
+	}
 	return $paging;
 }
 
@@ -294,7 +301,7 @@ sub clean_locus {
 	my $set_id     = $self->get_set_id;
 	if ($set_id) {
 		my $set_name = $self->{'datastore'}->run_query(
-			"SELECT set_name FROM set_loci WHERE set_id=? AND locus=?",
+			'SELECT set_name FROM set_loci WHERE set_id=? AND locus=?',
 			[ $set_id, $locus ],
 			{ fetch => 'row_array', cache => 'clean_locus' }
 		);
@@ -309,7 +316,9 @@ sub check_isolate_is_valid {
 	if ( !BIGSdb::Utils::is_int($isolate_id) ) {
 		send_error( 'Isolate id must be an integer', 400 );
 	}
-	my $exists = $self->{'datastore'}->run_query( "SELECT EXISTS(SELECT * FROM $self->{'system'}->{'view'} WHERE id=?)", $isolate_id );
+	my $exists =
+	  $self->{'datastore'}
+	  ->run_query( "SELECT EXISTS(SELECT * FROM $self->{'system'}->{'view'} WHERE id=?)", $isolate_id );
 	if ( !$exists ) {
 		send_error( "Isolate $isolate_id does not exist.", 404 );
 	}

lib/BIGSdb/REST/Routes/Resources.pm
@@ -21,10 +21,9 @@ use strict;
 use warnings;
 use 5.010;
 use Dancer2 appname => 'BIGSdb::REST::Interface';
-
-get '/robots.txt' => sub {
-  content_type "text/plain";
-  return "User-agent: *\nDisallow: /";
+get '/robots.txt'   => sub {
+	content_type 'text/plain';
+	return "User-agent: *\nDisallow: /";
 };
 
 #Resource description routes
@@ -51,7 +50,7 @@ any [qw(get post)] => '/' => sub {
 	}
 	return $values;
 };
-any [qw(get post)] => qr{^/db/?+$} => sub {
+any [qw(get post)] => qr{^/db/?+$}x => sub {
 	redirect '/';
 };
 any [qw(get post)] => '/db/:db' => sub {

lib/BIGSdb/REST/Routes/Schemes.pm
@@ -32,7 +32,10 @@ any [qw(get post)] => '/db/:db/schemes' => sub {
 	my $values  = [];
 	foreach my $scheme (@$schemes) {
 		push @$values,
-		  { scheme => request->uri_for("/db/$db/schemes/$scheme->{'id'}")->as_string, description => $scheme->{'description'} };
+		  {
+			scheme      => request->uri_for("/db/$db/schemes/$scheme->{'id'}")->as_string,
+			description => $scheme->{'description'}
+		  };
 	}
 	return $values;
 };
@@ -46,7 +49,8 @@ any [qw(get post)] => '/db/:db/schemes/:scheme' => sub {
 	$values->{'id'}                    = int($scheme_id);
 	$values->{'description'}           = $scheme_info->{'description'};
 	$values->{'has_primary_key_field'} = $scheme_info->{'primary_key'} ? JSON::true : JSON::false;
-	$values->{'primary_key_field'}     = request->uri_for("/db/$db/schemes/$scheme_id/fields/$scheme_info->{'primary_key'}")->as_string
+	$values->{'primary_key_field'} =
+	  request->uri_for("/db/$db/schemes/$scheme_id/fields/$scheme_info->{'primary_key'}")->as_string
 	  if $scheme_info->{'primary_key'};
 	my $scheme_fields      = $self->{'datastore'}->get_scheme_fields($scheme_id);
 	my $scheme_field_links = [];
@@ -67,8 +71,8 @@ any [qw(get post)] => '/db/:db/schemes/:scheme' => sub {
 		my $profile_view =
 		  ( $self->{'system'}->{'materialized_views'} // '' ) eq 'yes' ? "mv_scheme_$scheme_id" : "scheme_$scheme_id";
 		my $profile_count = $self->{'datastore'}->run_query("SELECT COUNT(*) FROM $profile_view");
-		$values->{'profile_count'} = int($profile_count);                                                 #Force integer output (non-quoted)
-		$values->{'profiles'}      = request->uri_for("/db/$db/schemes/$scheme_id/profiles")->as_string;
+		$values->{'profile_count'} = int($profile_count);    #Force integer output (non-quoted)
+		$values->{'profiles'}     = request->uri_for("/db/$db/schemes/$scheme_id/profiles")->as_string;
 		$values->{'profiles_csv'} = request->uri_for("/db/$db/schemes/$scheme_id/profiles_csv")->as_string;
 	}
 	return $values;

lib/BIGSdb/REST/Routes/Users.pm
@@ -30,7 +30,8 @@ any [qw(get post)] => '/db/:db/users/:user' => sub {
 	if ( !BIGSdb::Utils::is_int($user_id) ) {
 		send_error( 'User id must be an integer.', 400 );
 	}
-	my $user = $self->{'datastore'}->run_query( "SELECT * FROM users WHERE id=?", $user_id, { fetch => 'row_hashref' } );
+	my $user =
+	  $self->{'datastore'}->run_query( 'SELECT * FROM users WHERE id=?', $user_id, { fetch => 'row_hashref' } );
 	if ( !defined $user->{'id'} ) {
 		send_error( "User $user_id does not exist.", 404 );
 	}

commit 0f01b87ae585de1ec2cd0c501e0d52c1c1e9fa95

Fix REST OAuth access token broken by commit 86c0889.

lib/BIGSdb/REST/Routes/OAuth.pm
@@ -166,7 +166,7 @@ sub _get_access_token {
 	my $access_token_secret = BIGSdb::Utils::random_string(32);
 	eval {
 		$self->{'auth_db'}
-		  ->do( 'UPDATE request_tokens SET (redeemed,token)=(?,?)', undef, 1, $request_token->{'token'} );
+		  ->do( 'UPDATE request_tokens SET redeemed=? WHERE token=?', undef, 1, $request_token->{'token'} );
 
 		#Replace existing access token for same user.
 		$self->{'auth_db'}->do(