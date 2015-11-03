3d1d065ab0a6694c2a9fb42fb05381f915ab3b47
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-11-03 08:07:02 +0000

    Reformat CurateBatchSetAlleleFlagsPage.pm.

--- lib/BIGSdb/CurateBatchSetAlleleFlagsPage.pm
+++ lib/BIGSdb/CurateBatchSetAlleleFlagsPage.pm
@@ -29,15 +29,15 @@ use BIGSdb::Constants qw(ALLELE_FLAGS);
 sub print_content {
 	my ($self) = @_;
 	my $q = $self->{'cgi'};
-	say "<h1>Batch set allele flags</h1>";
+	say q(<h1>Batch set allele flags</h1>);
 	my $query_file = $q->param('query_file');
 	if ( !$query_file ) {
-		say "<div class=\"box\" id=\"statusbad\"><p>No query file passed.</p>";
+		say q(<div class="box" id="statusbad"><p>No query file passed.</p>);
 		return;
 	}
 	my $qry = $self->get_query_from_temp_file($query_file);
 	if ( !$qry ) {
-		say "<div class=\"box\" id=\"statusbad\"><p>No query passed.</p>";
+		say q(<div class="box" id="statusbad"><p>No query passed.</p>);
 		return;
 	}
 	my $alleles    = $self->{'datastore'}->run_query( $qry, undef, { fetch => 'all_arrayref', slice => {} } );
@@ -46,7 +46,8 @@ sub print_content {
 	if ( !$self->is_admin ) {
 		foreach my $locus ( keys %$loci ) {
 			if ( !$self->{'datastore'}->is_allowed_to_modify_locus_sequences( $locus, $curator_id ) ) {
-				say qq(<div class="box" id="statusbad"><p>Your user account isn't allowed to modify $locus sequences.</p>);
+				say q(<div class="box" id="statusbad"><p>Your user account isn't allowed )
+				  . qq(to modify $locus sequences.</p>);
 				return;
 			}
 		}
@@ -62,31 +63,34 @@ sub print_content {
 sub get_title {
 	my ($self) = @_;
 	my $desc = $self->{'system'}->{'description'} || 'BIGSdb';
-	return "Batch set allele flags - $desc";
+	return qq(Batch set allele flags - $desc);
 }
 
 sub _print_interface {
 	my ( $self, $alleles ) = @_;
 	my $q = $self->{'cgi'};
-	say "<div class=\"box\" id=\"queryform\">";
-	say "<p>Select the flags to set - this will override all existing flags on these alleles. "
-	  . "Clear all checkboxes to remove existing flags.</p>";
-	say "<p>" . @$alleles . " allele" . ( @$alleles > 1 ? 's' : '' ) . " selected.</p>";
+	say q(<div class="box" id="queryform">);
+	say q(<p>Select the flags to set - this will override all existing flags on these alleles. )
+	  . q(Clear all checkboxes to remove existing flags.</p>);
+	my $count = @$alleles;
+	my $plural = @$alleles > 1 ? q(s) : q();
+	say qq(<p>$count allele$plural selected.</p>);
 	say $q->start_form;
-	say qq(<fieldset style="float:left"><legend>Flags</legend>);
+	say q(<fieldset style="float:left"><legend>Flags</legend>);
 	say $q->checkbox_group( -name => 'flags', -values => [ALLELE_FLAGS], -linebreak => 'true' );
-	say "</fieldset>";
+	say q(</fieldset>);
 	$self->print_action_fieldset( { no_reset => 1, submit_label => 'Set' } );
 	say $q->hidden($_) foreach qw (db page query_file);
 	say $q->end_form;
-	say "</div>";
+	say q(</div>);
 	return;
 }
 
 sub _update {
 	my ( $self, $alleles ) = @_;
-	my $sql_delete = $self->{'db'}->prepare("DELETE FROM allele_flags WHERE locus=? AND allele_id=?");
-	my $sql_insert = $self->{'db'}->prepare("INSERT INTO allele_flags (locus,allele_id,flag,curator,datestamp) VALUES (?,?,?,?,?)");
+	my $sql_delete = $self->{'db'}->prepare('DELETE FROM allele_flags WHERE locus=? AND allele_id=?');
+	my $sql_insert =
+	  $self->{'db'}->prepare('INSERT INTO allele_flags (locus,allele_id,flag,curator,datestamp) VALUES (?,?,?,?,?)');
 	my $curator_id = $self->get_curator_id;
 	eval {
 		foreach my $allele (@$alleles)
@@ -99,12 +103,13 @@ sub _update {
 		}
 	};
 	if ($@) {
-		say "<div class=\"box\" id=\"statusbad\"><p>Update failed.</p></div>";
+		say q(<div class="box" id="statusbad"><p>Update failed.</p></div>);
 		$logger->error($@);
 		$self->{'db'}->rollback;
 	} else {
-		say "<div class=\"box\" id=\"resultsheader\"><p>Flags updated.</p>";
-		say "<p><a href=\"$self->{'system'}->{'script_name'}?db=$self->{'instance'}\">Back to main page</a></p></div>";
+		say q(<div class="box" id="resultsheader"><p>Flags updated.</p>);
+		say qq(<p><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}">)
+		  . q(Back to main page</a></p></div>);
 		$self->{'db'}->commit;
 	}
 	return;
