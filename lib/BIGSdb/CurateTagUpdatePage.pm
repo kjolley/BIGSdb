1c90354161cd10849aab47d75b3791039eb45669
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-11-04 14:56:51 +0000

    Reformat/refactor CurateTagUpdatePage.pm.

--- lib/BIGSdb/CurateTagUpdatePage.pm
+++ lib/BIGSdb/CurateTagUpdatePage.pm
@@ -24,41 +24,27 @@ use parent qw(BIGSdb::CuratePage BIGSdb::ExtractedSequencePage);
 use BIGSdb::Utils;
 use Log::Log4perl qw(get_logger);
 my $logger = get_logger('BIGSdb.Page');
-use BIGSdb::Constants qw(SEQ_FLAGS);
+use BIGSdb::Constants qw(SEQ_FLAGS BUTTON_CLASS);
 use List::MoreUtils qw(none);
 
 sub print_content {
 	my ($self) = @_;
 	my $q      = $self->{'cgi'};
 	my $id     = $q->param('id');
-	say "<h1>Update sequence tag</h1>";
+	say q(<h1>Update sequence tag</h1>);
 	if ( !BIGSdb::Utils::is_int($id) ) {
-		say qq(<div class="box" id="statusbad"><p>Tag id must be an integer.</p></div>);
+		say q(<div class="box" id="statusbad"><p>Tag id must be an integer.</p></div>);
 		return;
 	}
-	my $existing_tag = $self->{'datastore'}->run_query( "SELECT * FROM allele_sequences WHERE id=?", $id, { fetch => 'row_hashref' } );
+	my $existing_tag =
+	  $self->{'datastore'}->run_query( 'SELECT * FROM allele_sequences WHERE id=?', $id, { fetch => 'row_hashref' } );
 	if ( !$existing_tag ) {
-		say qq(<div class="box" id="statusbad"><p>Tag does not exist.</p></div>);
+		say q(<div class="box" id="statusbad"><p>Tag does not exist.</p></div>);
 		return;
 	}
 	my ( $seqbin_id, $locus, $orig_start, $orig_end ) = @{$existing_tag}{qw(seqbin_id locus start_pos end_pos)};
 	my $cleaned_locus = $self->clean_locus($locus);
-	if ( $q->param('update') || $q->param('submit') ) {
-		if ( !defined $q->param('new_start') || !BIGSdb::Utils::is_int( $q->param('new_start') ) ) {
-			say qq(<div class="box" id="statusbad"><p>The start position must be an integer.  Resetting to initial values.</p></div>);
-			$q->param( update => 0 );
-			$q->param( submit => 0 );
-		} elsif ( !defined $q->param('new_end') || !BIGSdb::Utils::is_int( $q->param('new_end') ) ) {
-			say qq(<div class="box" id="statusbad"><p>The end position must be an integer.  Resetting to initial values.</p></div>);
-			$q->param( update => 0 );
-			$q->param( submit => 0 );
-		} elsif ( $q->param('new_start') && $q->param('new_start') && $q->param('new_start') > $q->param('new_end') ) {
-			say qq(<div class="box" id="statusbad"><p>The end position must be greater than the start.  Resetting to initial )
-			  . "values.</p></div>";
-			$q->param( update => 0 );
-			$q->param( submit => 0 );
-		}
-	}
+	$self->_check_values;
 	my ( $start, $end, $reverse, $complete );
 	if ( $q->param('update') || $q->param('submit') ) {
 		$start    = $q->param('new_start');
@@ -80,31 +66,34 @@ sub print_content {
 		my $reverse_flag  = $reverse  ? 'true' : 'false';
 		my $complete_flag = $complete ? 'true' : 'false';
 		my $curator_id    = $self->get_curator_id;
-		( my $clean_locus = $locus ) =~ s/'/\\'/g;
 		push @actions,
 		  {
-			statement => "UPDATE allele_sequences SET (start_pos,end_pos,reverse,complete,curator,datestamp) = "
-			  . "(?,?,?,?,?,?) WHERE id=?",
+			statement => 'UPDATE allele_sequences SET (start_pos,end_pos,reverse,complete,curator,datestamp)='
+			  . '(?,?,?,?,?,?) WHERE id=?',
 			arguments => [ $start, $end, $reverse_flag, $complete_flag, $curator_id, 'now', $id ]
 		  };
 		my $existing_flags = $self->{'datastore'}->get_sequence_flags($id);
 		my @new_flags      = $q->param('flags');
-
 		foreach my $new_flag (@new_flags) {
+
 			if ( !@$existing_flags || none { $new_flag eq $_ } @$existing_flags ) {
 				push @actions,
 				  {
-					statement => "INSERT INTO sequence_flags (id,flag,datestamp,curator) VALUES (?,?,?,?)",
+					statement => 'INSERT INTO sequence_flags (id,flag,datestamp,curator) VALUES (?,?,?,?)',
 					arguments => [ $id, $new_flag, 'now', $curator_id ]
 				  };
 			}
 		}
 		foreach my $existing_flag (@$existing_flags) {
 			if ( !@new_flags || none { $existing_flag eq $_ } @new_flags ) {
-				push @actions, { statement => "DELETE FROM sequence_flags WHERE id=? AND flag=?", arguments => [ $id, $existing_flag ] };
+				push @actions,
+				  {
+					statement => 'DELETE FROM sequence_flags WHERE id=? AND flag=?',
+					arguments => [ $id, $existing_flag ]
+				  };
 			}
 		}
-		local $" = '<br />';
+		local $" = q(<br />);
 		eval {
 			foreach my $action (@actions)
 			{
@@ -114,23 +103,29 @@ sub print_content {
 		if ($@) {
 			my $error = $@;
 			if ( $error =~ /duplicate/ ) {
-				say qq(<div class="box" id="statusbad"><p>Update failed - a tag already exists for this locus between postions $start )
-				  . qq(and $end on sequence seqbin#$seqbin_id</p><p><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}">)
-				  . "Back to main page</a></p></div>";
+				say q(<div class="box" id="statusbad"><p>Update failed - a tag already exists for this )
+				  . qq(locus between postions $start and $end on sequence seqbin#$seqbin_id</p>)
+				  . qq(<p><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}">)
+				  . q(Back to main page</a></p></div>);
 			} else {
-				say qq(<div class="box" id="statusbad"><p>Update failed - transaction cancelled - no records have been touched.</p>)
-				  . qq(<p><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}">Back to main page</a></p></div>);
+				say q(<div class="box" id="statusbad"><p>Update failed - transaction cancelled - )
+				  . q(no records have been touched.</p>)
+				  . qq(<p><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}">Back to main page</a>)
+				  . q(</p></div>);
 				$logger->error($error);
 			}
 			$self->{'db'}->rollback;
 		} else {
 			$self->{'db'}->commit;
-			say qq(<div class="box" id="resultsheader"><p>Sequence tag updated!</p><p><a href="$self->{'system'}->{'script_name'}?)
-			  . qq(db=$self->{'instance'}">Back to main page</a></p></div>);
-			local $" = '<br />';
-			my $isolate_id = $self->{'datastore'}->run_query( "SELECT isolate_id FROM sequence_bin WHERE id=?", $seqbin_id );
+			say q(<div class="box" id="resultsheader"><p>Sequence tag updated!</p>)
+			  . qq(<p><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}">)
+			  . q(Back to main page</a></p></div>);
+			local $" = q(<br />);
+			my $isolate_id =
+			  $self->{'datastore'}->run_query( 'SELECT isolate_id FROM sequence_bin WHERE id=?', $seqbin_id );
 			if ( defined $isolate_id ) {
-				$self->update_history( $isolate_id, "$locus: sequence tag updated. Seqbin id: $seqbin_id; $start-$end" );
+				$self->update_history( $isolate_id,
+					"$locus: sequence tag updated. Seqbin id: $seqbin_id; $start-$end" );
 			}
 			$q->param( start_pos => $q->param('new_start') );
 			$q->param( end_pos   => $q->param('new_end') );
@@ -138,71 +133,108 @@ sub print_content {
 			$orig_end   = $q->param('new_end');
 		}
 	}
-	say qq(<div class="box" id="queryform"><div class="scrollable">);
+	say q(<div class="box" id="queryform"><div class="scrollable">);
 	say $q->start_form;
-	say qq(<fieldset style="float:left"><legend>Tag</legend>);
+	say q(<fieldset style="float:left"><legend>Tag</legend>);
 	say qq(<dl class="data"><dt>seqbin id</dt><dd>$seqbin_id</dd>);
-	say "<dt>locus</dt><dd>$cleaned_locus</dd>";
+	say qq(<dt>locus</dt><dd>$cleaned_locus</dd>);
 	my $curator_name = $self->get_curator_name;
-	say "<dt>curator</dt><dd>$curator_name</dd>";
+	say qq(<dt>curator</dt><dd>$curator_name</dd>);
 	my $datestamp = BIGSdb::Utils::get_datestamp();
-	say "<dt>datestamp</dt><dd>$datestamp</dd>";
-	say "<dt>start</dt><dd>";
+	say qq(<dt>datestamp</dt><dd>$datestamp</dd>);
+	say q(<dt>start</dt><dd>);
 	say $q->textfield( -name => 'new_start', default => $start, -size => 10 );
-	say "</dd><dt>end</dt><dd>";
+	say q(</dd><dt>end</dt><dd>);
 	say $q->textfield( -name => 'new_end', default => $end, -size => 10 );
-	say "</dd><dt>reverse</dt><dd>";
+	say q(</dd><dt>reverse</dt><dd>);
 	say $q->checkbox( -name => 'new_reverse', -label => '', -value => 1, -checked => $reverse );
-	say "</dd><dt>complete</dt><dd>";
+	say q(</dd><dt>complete</dt><dd>);
 	say $q->checkbox( -name => 'new_complete', -label => '', -value => 1, -checked => $complete );
-	say "</dd></dl></fieldset>";
+	say q(</dd></dl></fieldset>);
 	my $flags = $self->{'datastore'}->get_sequence_flags($id);
-	say qq(<fieldset style="float:left"><legend>Flags</legend>);
-	say $q->scrolling_list( -name => 'flags', -id => 'flags', -values => [SEQ_FLAGS], -default => $flags, -size => 5, -multiple => 'true' );
-	say qq(<p class="comment">Select/deselect multiple flags<br />by holding down Shift or Ctrl<br />while clicking with the mouse.</p>);
-	say "</fieldset>";
-	say qq(<fieldset style="float:left"><legend>Display</legend>);
-	say $q->submit(
-		-name  => 'update',
-		-label => 'Update display',
-		-class => 'submitbutton ui-button ui-widget ui-state-default ui-corner-all'
+	say q(<fieldset style="float:left"><legend>Flags</legend>);
+	say $q->scrolling_list(
+		-name     => 'flags',
+		-id       => 'flags',
+		-values   => [SEQ_FLAGS],
+		-default  => $flags,
+		-size     => 5,
+		-multiple => 'true'
 	);
-	say "</fieldset>";
+	say q(<p class="comment">Select/deselect multiple flags<br />)
+	  . q(by holding down Shift or Ctrl<br />)
+	  . q(while clicking with the mouse.</p>);
+	say q(</fieldset>);
+	say q(<fieldset style="float:left"><legend>Display</legend>);
+	say $q->submit( -name => 'update', -label => 'Update display', -class => BUTTON_CLASS );
+	say q(</fieldset>);
 	$self->print_action_fieldset( { id => $id } );
 	say $q->hidden($_) foreach qw(db page id seqbin_id locus start_pos end_pos reverse);
 	say $q->end_form;
-	say "</div></div>";
-	say "<div class=\"box\" id=\"sequence\">";
-	my $flanking = $self->{'prefs'}->{'flanking'} || 100;
+	say q(</div></div>);
+	say q(<div class="box" id="sequence">);
+	my $flanking = $self->{'prefs'}->{'flanking'} // 100;
 	my $length = abs( $end - $start + 1 );
-	say "<p class=\"seq\" style=\"text-align:left\">";
+	say q(<p class="seq" style="text-align:left">);
 	my $locus_info = $self->{'datastore'}->get_locus_info($locus);
 	my $translate  = $locus_info->{'coding_sequence'} ? 1 : 0;
 	my $orf        = $locus_info->{'orf'} || 1;
 	my $display    = $self->format_seqbin_sequence(
-		{ seqbin_id => $seqbin_id, reverse => $reverse, start => $start, end => $end, translate => $translate, orf => $orf } );
+		{
+			seqbin_id => $seqbin_id,
+			reverse   => $reverse,
+			start     => $start,
+			end       => $end,
+			translate => $translate,
+			orf       => $orf
+		}
+	);
 	say $display->{'seq'};
-	say "</p>";
+	say q(</p>);
 
 	if ($translate) {
 		my @stops = @{ $display->{'internal_stop'} };
 		if (@stops) {
 			local $" = ', ';
 			my $plural = @stops == 1 ? '' : 's';
-			print qq[<span class="highlight">Internal stop codon$plural at position$plural: @stops (numbering includes upstream ]
-			  . qq[flanking sequence).</span>];
+			print qq(<span class="highlight">Internal stop codon$plural at position$plural: )
+			  . qq(@stops (numbering includes upstream flanking sequence).</span>);
 		}
-		say "<pre class=\"sixpack\">";
+		say q(<pre class="sixpack">);
 		say $display->{'sixpack'};
-		say "</pre>";
+		say q(</pre>);
 	}
-	say "</div>";
+	say q(</div>);
+	return;
+}
+
+sub _check_values {
+	my ($self) = @_;
+	my $q = $self->{'cgi'};
+	if ( $q->param('update') || $q->param('submit') ) {
+		if ( !defined $q->param('new_start') || !BIGSdb::Utils::is_int( $q->param('new_start') ) ) {
+			say q(<div class="box" id="statusbad"><p>The start position must be an integer. )
+			  . q(Resetting to initial values.</p></div>);
+			$q->param( update => 0 );
+			$q->param( submit => 0 );
+		} elsif ( !defined $q->param('new_end') || !BIGSdb::Utils::is_int( $q->param('new_end') ) ) {
+			say q(<div class="box" id="statusbad"><p>The end position must be an integer. )
+			  . q(Resetting to initial values.</p></div>);
+			$q->param( update => 0 );
+			$q->param( submit => 0 );
+		} elsif ( $q->param('new_start') && $q->param('new_start') && $q->param('new_start') > $q->param('new_end') ) {
+			say q(<div class="box" id="statusbad"><p>The end position must be greater than the start. )
+			  . q(Resetting to initial values.</p></div>);
+			$q->param( update => 0 );
+			$q->param( submit => 0 );
+		}
+	}
 	return;
 }
 
 sub get_title {
 	my ($self) = @_;
 	my $desc = $self->{'system'}->{'description'} || 'BIGSdb';
-	return "Sequence tag update - $desc";
+	return qq(Sequence tag update - $desc);
 }
 1;
