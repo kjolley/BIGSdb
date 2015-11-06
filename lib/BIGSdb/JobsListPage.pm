c65857fc1c0c583104c5d340f9d03d60a3d30aad
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-11-06 11:45:07 +0000

    Reformat/refactor JobsListPage.pm.

--- lib/BIGSdb/JobsListPage.pm
+++ lib/BIGSdb/JobsListPage.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#Copyright (c) 2014, University of Oxford
+#Copyright (c) 2014-2015, University of Oxford
 #E-mail: keith.jolley@zoo.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -47,28 +47,28 @@ sub print_content {
 	my $desc = $self->get_db_description;
 	my $days = $self->{'config'}->{'results_deleted_days'} // 7;
 	my $days_plural = $days == 1 ? '' : 's';
-	say "<h1>Jobs - $desc database</h1>";
+	say qq(<h1>Jobs - $desc database</h1>);
 	my $jobs = $self->{'jobManager'}->get_user_jobs( $self->{'instance'}, $self->{'username'}, $days );
 	my $user = $self->{'datastore'}->get_user_info_from_username( $self->{'username'} );
 	if ( !keys %$user ) {
-		say "<p>No information about current user.</p>";
+		say q(<p>No information about current user.</p>);
 		return;
 	}
-	eval "use Time::Duration";    ## no critic (ProhibitStringyEval)
+	eval 'use Time::Duration';    ## no critic (ProhibitStringyEval)
 	my $use_time_duration    = 1;
-	my $nice_duration_header = '<th class="{sorter: false}">Duration (description)</th>';
+	my $nice_duration_header = q(<th class="{sorter: false}">Duration (description)</th>);
 	if ($@) {
 		$use_time_duration    = 0;
-		$nice_duration_header = '';
+		$nice_duration_header = q();
 	}
-	say "<div class=\"box\" id=\"resultstable\"><div class=\"scrollable\">";
-	say "<p>This page shows all offline jobs run by you in the past $days day$days_plural.<p>";
-	say "<h2>User: $user->{'first_name'} $user->{'surname'}</h2>";
-	say "<p>Click on the job id to see results.  You can also cancel queued and running jobs.</p>";
-	say "<table class=\"tablesorter\" id=\"sortTable\"><thead><tr><th class=\"{sorter: false}\">Job</th><th>Analysis</th>"
-	  . "<th class=\"{sorter: false}\">Size</th><th>Submitted</th><th>Started</th><th>Finished</th><th>Duration (seconds)</th>"
-	  . "$nice_duration_header<th>Status</th><th>Progress (%)</th><th>Stage</th></tr></thead>";
-	say "<tbody>";
+	say q(<div class="box" id="resultstable"><div class="scrollable">);
+	say qq(<p>This page shows all offline jobs run by you in the past $days day$days_plural.<p>);
+	say qq(<h2>User: $user->{'first_name'} $user->{'surname'}</h2>);
+	say q(<p>Click on the job id to see results. You can also cancel queued and running jobs.</p>);
+	say q(<table class="tablesorter" id="sortTable"><thead><tr><th class="{sorter: false}">Job</th>)
+	  . q(<th>Analysis</th><th class="{sorter: false}">Size</th><th>Submitted</th><th>Started</th>)
+	  . qq(<th>Finished</th><th>Duration (seconds)</th>$nice_duration_header<th>Status</th>)
+	  . q(<th>Progress (%)</th><th>Stage</th></tr></thead><tbody>);
 	foreach my $job (@$jobs) {
 
 		if ( $job->{'total_time'} ) {
@@ -80,31 +80,33 @@ sub print_content {
 			$job->{'duration'} = duration( $job->{'duration_s'} );
 			$job->{'duration'} = '<1 second' if $job->{'duration'} eq 'just now';
 		}
-		if ( $job->{'status'} =~ /^rejected/ ) {
-			$job->{'status'} =~
-		  s/(BIGSdb_\d+_\d+_\d+)/<a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=job&amp;id=$1">$1<\/a>/;
+		if ( $job->{'status'} =~ /^rejected/x ) {
+			my $url = qq($self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=job);
+			$job->{'status'} =~ s/(BIGSdb_\d+_\d+_\d+)/<a href="$url&amp;id=$1">$1<\/a>/x;
 		}
-		print "<tr>";
+		say q(<tr>);
 		my $size_stats = $self->_get_job_size_stats( $job->{'id'} );
-		local $" = '; ';
-		$job->{'size'} = "@$size_stats";
-		foreach my $field (qw (id module size submit_time start_time stop_time duration_s duration status percent_complete stage)) {
+		local $" = q(; );
+		$job->{'size'} = qq(@$size_stats);
+		foreach my $field (
+			qw (id module size submit_time start_time stop_time duration_s duration status percent_complete stage))
+		{
 			next if $field eq 'duration' && !$use_time_duration;
 			$job->{$field} //= '';
-			$job->{$field} = substr( $job->{$field}, 0, 16 ) if $field =~ /time$/;
+			$job->{$field} = substr( $job->{$field}, 0, 16 ) if $field =~ /time$/x;
 			if ( $field eq 'id' ) {
-				print "<td><a href=\"$self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=job&amp;id=$job->{'id'}\">"
-				  . "$job->{'id'}</a></td>";
+				say qq(<td><a href="$self->{'system'}->{'script_name'}?db=$self->{'instance'}&amp;page=job&amp;)
+				  . qq(id=$job->{'id'}">$job->{'id'}</a></td>);
 			} else {
-				print "<td>$job->{$field}</td>";
+				say qq(<td>$job->{$field}</td>);
 			}
 		}
-		say "</tr>";
+		say q(</tr>);
 	}
-	say "</tbody></table>";
-	say "</div></div><div class=\"box\" id=\"resultsfooter\">";
-	say "<p>This page will refresh every 60 seconds.</p>";
-	say "</div>";
+	say q(</tbody></table>);
+	say q(</div></div><div class="box" id="resultsfooter">);
+	say q(<p>This page will refresh every 60 seconds.</p>);
+	say q(</div>);
 	return;
 }
 
@@ -113,19 +115,19 @@ sub _get_job_size_stats {
 	my $params = $self->{'jobManager'}->get_job_params($job_id);
 	my @size_stats;
 	my $isolates = $self->{'jobManager'}->get_job_isolates($job_id);
-	push @size_stats, @$isolates . " isolate" . ( @$isolates == 1 ? '' : 's' ) if @$isolates;
+	push @size_stats, @$isolates . q( isolate) . ( @$isolates == 1 ? q() : q(s) ) if @$isolates;
 	if ( $params->{'scheme_id'} ) {
 		my $profiles = $self->{'jobManager'}->get_job_profiles( $job_id, $params->{'scheme_id'} );
-		push @size_stats, @$profiles . " profile" . ( @$profiles == 1 ? '' : 's' ) if @$profiles;
+		push @size_stats, @$profiles . q( profile) . ( @$profiles == 1 ? q() : q(s) ) if @$profiles;
 	}
 	my $loci = $self->{'jobManager'}->get_job_loci($job_id);
-	push @size_stats, @$loci . " loc" . ( @$loci == 1 ? 'us' : 'i' ) if @$loci;
+	push @size_stats, @$loci . q( loc) . ( @$loci == 1 ? q(us) : q(i) ) if @$loci;
 	return \@size_stats;
 }
 
 sub get_title {
 	my ($self) = @_;
 	my $desc = $self->{'system'}->{'description'} || 'BIGSdb';
-	return "Offline job list - $desc";
+	return qq(Offline job list - $desc);
 }
 1;
