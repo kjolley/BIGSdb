6dbc9e1c9668f59b3dc560b4def3d4b125b0e6c9
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2015-12-14 07:06:24 +0000

    Refactor AutoTag.pm.

--- lib/BIGSdb/Offline/AutoTag.pm
+++ lib/BIGSdb/Offline/AutoTag.pm
@@ -29,28 +29,14 @@ use constant TAG_USERNAME      => 'autotagger';
 use constant DEFAULT_WORD_SIZE => 60;             #Only looking for exact matches
 use constant MISSING_ALLELE_ALIGNMENT => 30;
 use constant MISSING_ALLELE_IDENTITY  => 50;
+use constant PROBLEM                  => 1;
 
 sub run_script {
 	my ($self) = @_;
 	return $self if $self->{'options'}->{'query_only'};    #Return script object to allow access to methods
 	my $EXIT = 0;
 	local @SIG{qw (INT TERM HUP)} = ( sub { $EXIT = 1 } ) x 3;    #Allow temp files to be cleaned on kill signals
-	my $params;
-	$params->{$_} = 1 foreach qw(pcr_filter probe_filter);
-	if ( BIGSdb::Utils::is_int( $self->{'options'}->{'w'} ) ) {
-		$params->{'word_size'} = $self->{'options'}->{'w'};
-	} else {
-		if ( $self->{'options'}->{'0'} ) {
-			$params->{'word_size'} = 15;                          #More stringent if checking for missing loci
-		} else {
-			$params->{'word_size'}          = DEFAULT_WORD_SIZE;
-			$params->{'exact_matches_only'} = 1;
-		}
-	}
-	if ( $self->{'options'}->{'0'} ) {
-		$params->{'alignment'} = MISSING_ALLELE_ALIGNMENT;
-		$params->{'identity'}  = MISSING_ALLELE_IDENTITY;
-	}
+	my $params = $self->_get_params;
 	die "No connection to database (check logs).\n" if !defined $self->{'db'};
 	die "This script can only be run against an isolate database.\n"
 	  if ( $self->{'system'}->{'dbtype'} // '' ) ne 'isolates';
@@ -82,6 +68,7 @@ sub run_script {
 			"$self->{'options'}->{'d'}#pid$$:Checking isolate $isolate_id - $i/" . (@$isolate_list) . "($complete%)" );
 		undef $self->{'history'};
 	  LOCUS: foreach my $locus (@$loci) {
+			last if $EXIT || $self->_is_time_up;
 			my $existing_allele_ids = $self->{'datastore'}->get_allele_ids( $isolate_id, $locus );
 			next if @$existing_allele_ids;
 			my $allele_seq = $self->{'datastore'}->get_allele_sequence( $isolate_id, $locus );
@@ -90,92 +77,144 @@ sub run_script {
 			my ( $exact_matches, $partial_matches ) =
 			  $self->blast( $params, $locus, $isolate_id, $isolate_prefix, $locus_prefix );
 			my $blast_status_bad = $?;
+
 			if ( ref $exact_matches && @$exact_matches ) {
-				print "Isolate: $isolate_id; Locus: $locus; " if !$self->{'options'}->{'q'};
-				foreach (@$exact_matches) {
-					if ( $_->{'allele'} ) {
-						print "Allele: $_->{'allele'} " if !$self->{'options'}->{'q'};
-						my $sender = $self->{'datastore'}->run_query( 'SELECT sender FROM sequence_bin WHERE id=?',
-							$_->{'seqbin_id'}, { cache => 'AutoTag::run_script_sender' } );
-						my $problem = 0;
-						try {
-							$self->_tag_allele(
-								{
-									isolate_id => $isolate_id,
-									locus      => $locus,
-									allele_id  => $_->{'allele'},
-									sender     => $sender
-								}
-							);
-							if ( !$self->{'options'}->{'T'} || !@$allele_seq ) {
-								$self->_tag_sequence(
-									{
-										seqbin_id => $_->{'seqbin_id'},
-										locus     => $locus,
-										allele_id => $_->{'allele'},
-										start_pos => $_->{'start'},
-										end_pos   => $_->{'end'},
-										reverse   => $_->{'reverse'}
-									}
-								);
-							}
-						}
-						catch BIGSdb::DatabaseException with {
-							$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$isolate_prefix*");
-							$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$locus_prefix*");
-							$problem = 1;
-						};
-						if ($problem) {
-							$self->_update_isolate_history( $isolate_id, $self->{'history'} );
-							last ISOLATE;
-						}
+				my $ret_val = $self->_handle_match(
+					{
+						isolate_id     => $isolate_id,
+						locus          => $locus,
+						exact_matches  => $exact_matches,
+						allele_seq     => $allele_seq,
+						isolate_prefix => $isolate_prefix,
+						locus_prefix   => $locus_prefix
 					}
-				}
-				print "\n" if !$self->{'options'}->{'q'};
+				);
+				last ISOLATE if $ret_val;
 			} elsif ( $self->{'options'}->{'0'} && !$blast_status_bad ) {
-				if ( ref $partial_matches && @$partial_matches ) {
-					foreach my $match (@$partial_matches) {
-						next LOCUS
-						  if $match->{'identity'} >= MISSING_ALLELE_IDENTITY
-						  && $match->{'alignment'} >= MISSING_ALLELE_ALIGNMENT;
+				my $ret_val = $self->_handle_no_match(
+					{
+						isolate_id      => $isolate_id,
+						locus           => $locus,
+						partial_matches => $partial_matches,
+						isolate_prefix  => $isolate_prefix,
+						locus_prefix    => $locus_prefix
 					}
-				}
-				say "Isolate: $isolate_id; Locus: $locus; Allele: 0 " if !$self->{'options'}->{'q'};
-				my $problem = 0;
-				try {
-					$self->_tag_allele(
-						{
-							isolate_id => $isolate_id,
-							locus      => $locus,
-							allele_id  => '0',
-							status     => 'provisional',
-							sender     => TAG_USER
-						}
-					);
-				}
-				catch BIGSdb::DatabaseException with {
-					$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$isolate_prefix*");
-					$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$locus_prefix*");
-					$problem = 1;
-				};
-				if ($problem) {
-					$self->_update_isolate_history( $isolate_id, $self->{'history'} );
-					last ISOLATE;
-				}
+				);
+				last ISOLATE if $ret_val;
 			}
-			last if $EXIT || $self->_is_time_up;
 		}
 		$self->_update_isolate_history( $isolate_id, $self->{'history'} );
-		$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$isolate_prefix*")
-		  ;    #delete isolate seqbin FASTA
+
+		#Delete isolate seqbin FASTA
+		$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$isolate_prefix*");
 		last if $EXIT || $self->_is_time_up;
 	}
-	$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$locus_prefix*");    #delete locus working files
+
+	#Delete locus working files
+	$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$locus_prefix*");
 	if ( $self->_is_time_up && !$self->{'options'}->{'q'} ) {
 		say "Time limit reached ($self->{'options'}->{'t'} minute"
 		  . ( $self->{'options'}->{'t'} == 1 ? '' : 's' ) . ')';
 	}
 	$self->{'logger'}->info("$self->{'options'}->{'d'}#pid$$:Autotagger stop");
+	return;
+}
+
+sub _get_params {
+	my ($self) = @_;
+	my $params = {};
+	$params->{$_} = 1 foreach qw(pcr_filter probe_filter);
+	if ( BIGSdb::Utils::is_int( $self->{'options'}->{'w'} ) ) {
+		$params->{'word_size'} = $self->{'options'}->{'w'};
+	} else {
+		if ( $self->{'options'}->{'0'} ) {
+			$params->{'word_size'} = 15;    #More stringent if checking for missing loci
+		} else {
+			$params->{'word_size'}          = DEFAULT_WORD_SIZE;
+			$params->{'exact_matches_only'} = 1;
+		}
+	}
+	if ( $self->{'options'}->{'0'} ) {
+		$params->{'alignment'} = MISSING_ALLELE_ALIGNMENT;
+		$params->{'identity'}  = MISSING_ALLELE_IDENTITY;
+	}
+	return;
+}
+
+sub _handle_match {
+	my ( $self, $args ) = @_;
+	my ( $isolate_id, $locus, $exact_matches, $allele_seq, $isolate_prefix, $locus_prefix ) =
+	  @{$args}{qw(isolate_id locus exact_matches allele_seq isolate_prefix locus_prefix)};
+	print "Isolate: $isolate_id; Locus: $locus; " if !$self->{'options'}->{'q'};
+	foreach (@$exact_matches) {
+		next if !$_->{'allele'};
+		print "Allele: $_->{'allele'} " if !$self->{'options'}->{'q'};
+		my $sender = $self->{'datastore'}->run_query( 'SELECT sender FROM sequence_bin WHERE id=?',
+			$_->{'seqbin_id'}, { cache => 'AutoTag::run_script_sender' } );
+		my $problem;
+		try {
+			$self->_tag_allele(
+				{ isolate_id => $isolate_id, locus => $locus, allele_id => $_->{'allele'}, sender => $sender } );
+			if ( !$self->{'options'}->{'T'} || !@$allele_seq ) {
+				$self->_tag_sequence(
+					{
+						seqbin_id => $_->{'seqbin_id'},
+						locus     => $locus,
+						allele_id => $_->{'allele'},
+						start_pos => $_->{'start'},
+						end_pos   => $_->{'end'},
+						reverse   => $_->{'reverse'}
+					}
+				);
+			}
+		}
+		catch BIGSdb::DatabaseException with {
+			$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$isolate_prefix*");
+			$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$locus_prefix*");
+			$problem = PROBLEM;
+		};
+		if ($problem) {
+			$self->_update_isolate_history( $isolate_id, $self->{'history'} );
+			return PROBLEM;
+		}
+	}
+	print "\n" if !$self->{'options'}->{'q'};
+	return;
+}
+
+sub _handle_no_match {
+	my ( $self, $args ) = @_;
+	my ( $isolate_id, $locus, $partial_matches, $isolate_prefix, $locus_prefix ) =
+	  @{$args}{qw(isolate_id locus partial_matches isolate_prefix locus_prefix)};
+	if ( ref $partial_matches && @$partial_matches ) {
+		foreach my $match (@$partial_matches) {
+			return
+			  if $match->{'identity'} >= MISSING_ALLELE_IDENTITY
+			  && $match->{'alignment'} >= MISSING_ALLELE_ALIGNMENT;
+		}
+	}
+	say "Isolate: $isolate_id; Locus: $locus; Allele: 0 " if !$self->{'options'}->{'q'};
+	my $problem = 0;
+	try {
+		$self->_tag_allele(
+			{
+				isolate_id => $isolate_id,
+				locus      => $locus,
+				allele_id  => '0',
+				status     => 'provisional',
+				sender     => TAG_USER
+			}
+		);
+	}
+	catch BIGSdb::DatabaseException with {
+		$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$isolate_prefix*");
+		$self->delete_temp_files("$self->{'config'}->{'secure_tmp_dir'}/*$locus_prefix*");
+		$problem = PROBLEM;
+	};
+	if ($problem) {
+		$self->_update_isolate_history( $isolate_id, $self->{'history'} );
+		return PROBLEM;
+	}
 	return;
 }
 
