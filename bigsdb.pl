5f87a20f9816c84896dcec6e875dee60dd1ee4e8
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2016-03-21 09:06:32 +0000

    Version 1.12.3.

--- bigsdb.pl
+++ bigsdb.pl
@@ -21,7 +21,7 @@
 package BIGSdb::main;
 use strict;
 use warnings;
-use version; our $VERSION = qv('v1.12.2');
+use version; our $VERSION = qv('v1.12.3');
 use 5.010;
 
 ###########Local configuration################################
