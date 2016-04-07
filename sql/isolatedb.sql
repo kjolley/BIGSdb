705778aa477949e3aecbe6e6827c3eccba0bd710
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2016-04-07 14:19:52 +0100

    Database changes to support linking tags to allele designations.

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -991,3 +991,22 @@ $check_isolate_defined$ LANGUAGE plpgsql;
 CREATE TRIGGER check_isolate_defined AFTER INSERT OR UPDATE ON retired_isolates
 	FOR EACH ROW
 	EXECUTE PROCEDURE check_isolate_defined();
+
+CREATE TABLE tag_designations (
+allele_sequence_id bigint NOT NULL,
+allele_designation_id bigint NOT NULL,
+curator int NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY(allele_sequence_id,allele_designation_id),
+CONSTRAINT td_allele_sequence_id FOREIGN KEY(allele_sequence_id) REFERENCES allele_sequences
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT td_allele_designation_id FOREIGN KEY(allele_designation_id) REFERENCES allele_designations
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT td_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON tag_designations TO apache;
