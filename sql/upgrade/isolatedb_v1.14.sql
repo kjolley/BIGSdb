2bd6da5b706ea3418623ebc1535417bce97a7150
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2016-04-19 10:59:49 +0100

    Store scheme indices in isolate database.

--- sql/upgrade/isolatedb_v1.14.sql
+++ sql/upgrade/isolatedb_v1.14.sql
@@ -1,2 +1,18 @@
 --Standardize all scheme tables to use mv_ prefix.
 UPDATE schemes SET dbase_table=REGEXP_REPLACE(dbase_table,'^scheme_','mv_scheme_') WHERE dbase_table IS NOT NULL;
+
+CREATE TABLE scheme_warehouse_indices (
+scheme_id int NOT NULL,
+locus text NOT NULL,
+index int NOT NULL,
+PRIMARY KEY (scheme_id,locus),
+CONSTRAINT swi_scheme_id FOREIGN KEY (scheme_id) REFERENCES schemes
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT swi_locus FOREIGN KEY (locus) REFERENCES loci
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON scheme_warehouse_indices TO apache;
+
