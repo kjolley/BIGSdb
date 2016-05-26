b0698dd1fdeba683443eb18d69e0f3ceffb06a01
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2016-05-26 13:59:11 +0100

    Cache scheme warehouse indices.

--- lib/BIGSdb/Datastore.pm
+++ lib/BIGSdb/Datastore.pm
@@ -376,8 +376,11 @@ sub get_scheme_locus_indices {
 
 sub get_scheme_warehouse_locus_name {
 	my ( $self, $scheme_id, $locus ) = @_;
-	my $locus_indices = $self->get_scheme_locus_indices( $scheme_id, { pg_numbering => 1 } );
-	return "profile[$locus_indices->{$locus}]";
+	if ( !$self->{'cache'}->{'scheme_warehouse_locus_indices'}->{$scheme_id} ) {
+		$self->{'cache'}->{'scheme_warehouse_locus_indices'}->{$scheme_id} =
+		  $self->get_scheme_locus_indices( $scheme_id, { pg_numbering => 1 } );
+	}
+	return "profile[$self->{'cache'}->{'scheme_warehouse_locus_indices'}->{$scheme_id}->{$locus}]";
 }
 
 #pk_value is optional and can be used to check if updating an existing profile matches another definition.
