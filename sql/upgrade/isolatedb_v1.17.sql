2c78a83f31d0dd96b0daa59626fd2e3acc830af4
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2017-02-17 09:22:43 +0000

    Tables for project users.

--- sql/upgrade/isolatedb_v1.17.sql
+++ sql/upgrade/isolatedb_v1.17.sql
@@ -5,3 +5,51 @@ UPDATE projects SET isolate_display=false WHERE isolate_display IS NULL;
 ALTER TABLE projects ALTER COLUMN isolate_display SET NOT NULL;
 UPDATE projects SET list=false WHERE list IS NULL;
 ALTER TABLE projects ALTER COLUMN list SET NOT NULL;
+
+--Not yet added to isolatedb.sql
+ALTER TABLE projects ADD
+
+CREATE TABLE project_users (
+project_id integer NOT NULL,
+user_id integer NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (project_id,user_id),
+CONSTRAINT pu_project FOREIGN KEY (project_id) REFERENCES projects
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pu_user FOREIGN KEY (user_id) REFERENCES users
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pu_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON project_users TO apache;
+
+CREATE TABLE project_user_groups (
+project_id integer NOT NULL,
+user_group integer NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (project_id,user_group),
+CONSTRAINT pug_project FOREIGN KEY (project_id) REFERENCES projects
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pug_usergroup FOREIGN KEY (user_group) REFERENCES user_groups
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pug_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON project_user_groups TO apache;
+
+CREATE VIEW merged_project_users AS
+SELECT project_id,user_id FROM project_users UNION 
+(SELECT project_id,user_id FROM project_user_groups AS pug LEFT JOIN 
+user_group_members ugm ON pug.user_group=ugm.user_group);
+
+GRANT SELECT ON merged_project_users TO apache;
