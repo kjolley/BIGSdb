6d52154156a1d9c5c192f4e5f9763246d0e98a44
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2017-02-25 16:05:23 +0000

    REST: Support extended fields when displaying used values.

--- lib/BIGSdb/REST/Routes/Fields.pm
+++ lib/BIGSdb/REST/Routes/Fields.pm
@@ -55,6 +55,25 @@ sub _get_fields {
 		}
 		$value->{'values'} = request->uri_for("/db/$db/field/$field");
 		push @$values, $value;
+		my $ext_att =
+		  $self->{'datastore'}
+		  ->run_query( 'SELECT * FROM isolate_field_extended_attributes WHERE isolate_field=? ORDER BY field_order',
+			$field, { fetch => 'all_arrayref', slice => {} } );
+		foreach my $att (@$ext_att) {
+			$att->{'value_format'} = 'int' if $att->{'value_format'} eq 'integer';
+			$value = {
+				name   => $att->{'attribute'},
+				values => request->uri_for("/db/$db/field/$att->{'attribute'}"),
+				type   => $att->{'value_format'}
+			};
+			$value->{'required'} = JSON::false;
+			$value->{'length'}   = $att->{'length'} if $att->{'length'};
+			$value->{'regex'}    = $att->{'value_regex'} if $att->{'value_regex'};
+			my $comments = qq(Value inferred from $field value.);
+			$comments .= qq( $att->{'description'}) if $att->{'description'};
+			$value->{'comments'} = $comments;
+			push @$values, $value;
+		}
 	}
 	return $values;
 }
@@ -64,6 +83,11 @@ sub _get_field {
 	my $params = params;
 	$self->check_isolate_database;
 	my ( $db, $field ) = @{$params}{qw(db field)};
+	my $is_extended_field = $self->{'datastore'}
+	  ->run_query( 'SELECT EXISTS(SELECT * FROM isolate_field_extended_attributes WHERE attribute=?)', $field );
+	if ($is_extended_field) {
+		return _get_extended_field($field);
+	}
 	if ( !$self->{'xmlHandler'}->is_field($field) ) {
 		send_error( "Field $field does not exist.", 404 );
 	}
@@ -72,10 +96,44 @@ sub _get_field {
 	my $page   = ( BIGSdb::Utils::is_int( param('page') ) && param('page') > 0 ) ? param('page') : 1;
 	my $pages  = ceil( $value_count / $self->{'page_size'} );
 	my $offset = ( $page - 1 ) * $self->{'page_size'};
-	my $qry =
-	  "SELECT DISTINCT $field FROM $self->{'system'}->{'view'} WHERE $field IS NOT NULL ORDER BY $field";
+	my $qry    = "SELECT DISTINCT $field FROM $self->{'system'}->{'view'} WHERE $field IS NOT NULL ORDER BY $field";
 	$qry .= " OFFSET $offset LIMIT $self->{'page_size'}" if !param('return_all');
 	my $set_values = $self->{'datastore'}->run_query( $qry, undef, { fetch => 'col_arrayref' } );
+	my $values = {
+		records => int($value_count),
+		values  => $set_values
+	};
+	my $paging = $self->get_paging( "/db/$db/field/$field", $pages, $page );
+	$values->{'paging'} = $paging if %$paging;
+	return $values;
+}
+
+sub _get_extended_field {
+	my $self   = setting('self');
+	my $params = params;
+	my ( $db, $field ) = @{$params}{qw(db field)};
+	my $ext_att =
+	  $self->{'datastore'}->run_query( 'SELECT * FROM isolate_field_extended_attributes WHERE attribute=? LIMIT 1',
+		$field, { fetch => 'row_hashref' } );
+	my $order;
+	foreach my $type (qw (integer float data)) {
+		if ( $ext_att->{'value_format'} eq $type ) {
+			$order = qq(CAST (value AS $type));
+		}
+	}
+	$order //= 'value';
+	my $count_qry =
+	    'SELECT COUNT(DISTINCT(value)) FROM isolate_value_extended_attributes WHERE (isolate_field,attribute)=(?,?) '
+	  . "AND field_value IN (SELECT DISTINCT($ext_att->{'isolate_field'}) FROM $self->{'system'}->{'view'})";
+	my $value_count = $self->{'datastore'}->run_query( $count_qry, [ $ext_att->{'isolate_field'}, $field ] );
+	my $page   = ( BIGSdb::Utils::is_int( param('page') ) && param('page') > 0 ) ? param('page') : 1;
+	my $pages  = ceil( $value_count / $self->{'page_size'} );
+	my $offset = ( $page - 1 ) * $self->{'page_size'};
+	my $qry    = 'SELECT DISTINCT(value) FROM isolate_value_extended_attributes WHERE (isolate_field,attribute)=(?,?) '
+	  . "AND field_value IN (SELECT DISTINCT($ext_att->{'isolate_field'}) FROM $self->{'system'}->{'view'}) ORDER BY $order";
+	$qry .= " OFFSET $offset LIMIT $self->{'page_size'}" if !param('return_all');
+	my $set_values =
+	  $self->{'datastore'}->run_query( $qry, [ $ext_att->{'isolate_field'}, $field ], { fetch => 'col_arrayref' } );
 	my $values = {
 		records => int($value_count),
 		values  => $set_values
