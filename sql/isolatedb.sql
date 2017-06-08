ef9486d5dd948166ecb39953c838edac82ad41a4
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2017-06-08 14:45:55 +0100

    Merge v1.17 db changes to isolatedb.sql

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -43,6 +43,7 @@ CREATE TABLE user_groups (
 id integer NOT NULL UNIQUE,
 description text NOT NULL UNIQUE,
 datestamp date NOT NULL,
+co_curate boolean NOT NULL,
 curator int NOT NULL,
 PRIMARY KEY (id),
 CONSTRAINT ug_curator FOREIGN KEY (curator) REFERENCES users
@@ -70,6 +71,23 @@ ON UPDATE CASCADE
 );
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON user_group_members TO apache;
+
+CREATE TABLE user_limits (
+user_id integer NOT NULL,
+attribute text NOT NULL,
+value integer NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (user_id,attribute),
+CONSTRAINT ul_user_id FOREIGN KEY (user_id) REFERENCES users
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT ul_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON user_limits TO apache;
 
 CREATE TABLE user_dbases (
 id int NOT NULL,
@@ -176,6 +194,7 @@ full_description text,
 isolate_display boolean NOT NULL,
 list boolean NOT NULL,
 private boolean NOT NULL,
+no_quota boolean NOT NULL,
 curator integer NOT NULL,
 datestamp date NOT NULL,
 PRIMARY KEY (id),
@@ -204,6 +223,70 @@ ON UPDATE CASCADE
 );
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON project_members TO apache;
+
+CREATE TABLE project_users (
+project_id integer NOT NULL,
+user_id integer NOT NULL,
+admin boolean NOT NULL,
+modify boolean NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (project_id,user_id),
+CONSTRAINT pu_project FOREIGN KEY (project_id) REFERENCES projects
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pu_user FOREIGN KEY (user_id) REFERENCES users
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pu_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON project_users TO apache;
+
+CREATE TABLE project_user_groups (
+project_id integer NOT NULL,
+user_group integer NOT NULL,
+modify boolean NOT NULL,
+curator integer NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (project_id,user_group),
+CONSTRAINT pug_project FOREIGN KEY (project_id) REFERENCES projects
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pug_usergroup FOREIGN KEY (user_group) REFERENCES user_groups
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pug_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON project_user_groups TO apache;
+
+CREATE VIEW merged_project_users AS SELECT project_id,user_id,bool_or(admin) AS admin,bool_or(modify) AS modify 
+FROM (SELECT project_id,user_id,admin,modify FROM project_users UNION ALL SELECT project_id,user_id,false,modify 
+FROM project_user_groups AS pug LEFT JOIN user_group_members ugm ON pug.user_group=ugm.user_group) AS merged 
+GROUP BY project_id,user_id;
+
+GRANT SELECT ON merged_project_users TO apache;
+
+CREATE TABLE private_isolates (
+isolate_id integer NOT NULL,
+user_id INTEGER NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (isolate_id),
+CONSTRAINT pi_isolate_id FOREIGN KEY (isolate_id) REFERENCES isolates
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT pi_user_id FOREIGN KEY (user_id) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON private_isolates TO apache;
+
 
 CREATE TABLE sequence_bin (
 id bigserial NOT NULL UNIQUE,
