3bcc54c225a91604b4264df330ac6295e2fc6ca2
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2018-03-22 10:02:52 +0000

    REST: Add method to return loci with new alleles
    Closes #455.

--- lib/BIGSdb/REST/Interface.pm
+++ lib/BIGSdb/REST/Interface.pm
@@ -188,11 +188,8 @@ sub _check_kiosk {
 	}
 	my $db = params->{'db'};
 	if ( $self->{'system'}->{'rest_kiosk'} eq 'sequenceQuery' ) {
-		my @allowed_routes = (
-			"POST /db/$db/loci/{locus}/sequence",
-			"POST /db/$db/sequence",
-			"POST /db/$db/schemes/{scheme_id}/sequence"
-		);
+		my @allowed_routes = ( "POST /db/$db/loci/{locus}/sequence", "POST /db/$db/sequence",
+			"POST /db/$db/schemes/{scheme_id}/sequence" );
 		local $" = q(, );
 		if ( request->method ne 'POST' ) {
 			send_error(
@@ -572,7 +569,8 @@ sub get_user_id {
 sub add_filters {
 	my ( $self, $qry, $allowed_args ) = @_;
 	my $params = params;
-	my ( $added_after, $updated_after ) = @{$params}{qw(added_after updated_after)};
+	my ( $added_after, $updated_after, $alleles_added_after, $alleles_updated_after ) =
+	  @{$params}{qw(added_after updated_after alleles_added_after alleles_updated_after)};
 	my @terms;
 	my %methods = (
 		added_after => sub {
@@ -580,6 +578,14 @@ sub add_filters {
 		},
 		updated_after => sub {
 			push @terms, qq(datestamp>'$updated_after') if BIGSdb::Utils::is_date($updated_after);
+		},
+		alleles_added_after => sub {
+			push @terms, qq(id IN (SELECT locus FROM sequences WHERE date_entered>'$alleles_added_after'))
+			  if BIGSdb::Utils::is_date($alleles_added_after);
+		},
+		alleles_updated_after => sub {
+			push @terms, qq(id IN (SELECT locus FROM locus_stats WHERE datestamp>'$alleles_updated_after'))
+			  if BIGSdb::Utils::is_date($alleles_updated_after);
 		}
 	);
 	foreach my $arg (@$allowed_args) {
