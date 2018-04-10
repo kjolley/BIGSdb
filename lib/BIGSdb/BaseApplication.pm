070a34da9c0e514a77579fe20c1bf2e11ce3aaf7
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2018-04-10 08:23:11 +0100

    Version 1.18.3.

--- lib/BIGSdb/BaseApplication.pm
+++ lib/BIGSdb/BaseApplication.pm
@@ -20,7 +20,7 @@ package BIGSdb::BaseApplication;
 use strict;
 use warnings;
 use 5.010;
-use version; our $VERSION = version->declare('v1.18.2');
+use version; our $VERSION = version->declare('v1.18.3');
 use BIGSdb::BIGSException;
 use BIGSdb::ClassificationScheme;
 use BIGSdb::Constants qw(:login_requirements);
