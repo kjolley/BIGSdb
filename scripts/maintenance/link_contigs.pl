c45ba9663c944d5d6dad7006dc7f719005710a9f
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2018-06-22 08:07:10 +0100

    link_contigs: Prepare insert statements outside loop.

--- scripts/maintenance/link_contigs.pl
+++ scripts/maintenance/link_contigs.pl
@@ -116,17 +116,18 @@ sub main {
 		'SELECT r.uri FROM remote_contigs r INNER JOIN sequence_bin s ON r.seqbin_id=s.id AND s.isolate_id=?',
 		$opts{'i'}, { fetch => 'col_arrayref' } );
 	my %existing = map { $_ => 1 } @$existing;
+	my $insert_sql =
+	  $script->{'db'}->prepare( 'INSERT INTO sequence_bin(isolate_id,remote_contig,sequence,sender,curator,'
+		  . 'date_entered,datestamp) VALUES (?,?,?,?,?,?,?) RETURNING id' );
+	my $insert_remote_sql = $script->{'db'}->prepare('INSERT INTO remote_contigs (seqbin_id,uri) VALUES (?,?)');
 	eval {
 		$script->{'db'}->do('ALTER TABLE sequence_bin DISABLE TRIGGER check_sequence_bin');
-		my $insert_sql =
-		  $script->{'db'}->prepare( 'INSERT INTO sequence_bin(isolate_id,remote_contig,sequence,sender,curator,'
-			  . 'date_entered,datestamp) VALUES (?,?,?,?,?,?,?) RETURNING id' );
+
 		foreach my $contig (@$contigs) {
 			next if $existing{$contig};    #Don't add duplicates
 			$insert_sql->execute( $opts{'i'}, 'true', '', $opts{'c'}, $opts{'c'}, 'now', 'now' );
 			my ($seqbin_id) = $insert_sql->fetchrow_array;
-			$script->{'db'}
-			  ->do( 'INSERT INTO remote_contigs (seqbin_id,uri) VALUES (?,?)', undef, $seqbin_id, $contig );
+			$insert_remote_sql->execute( $seqbin_id, $contig );
 		}
 		$script->{'db'}->do('ALTER TABLE sequence_bin ENABLE TRIGGER check_sequence_bin');
 	};
