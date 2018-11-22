7cfa313a72cf80630180505107f5f4516e3a1732
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2018-11-22 15:14:11 +0000

    Faster clustering function

--- sql/seqdef.sql
+++ sql/seqdef.sql
@@ -1377,9 +1377,9 @@ CREATE OR REPLACE FUNCTION matching_profiles_cg(i_cg_scheme_id int, profile_id1 
 		SELECT cs.scheme_id INTO i_scheme_id FROM classification_schemes cs WHERE id=i_cg_scheme_id;	
 		SELECT COUNT(*) INTO locus_count FROM scheme_members WHERE scheme_id=i_scheme_id;	
 		RETURN QUERY SELECT p2.profile_id FROM profile_members AS p1 JOIN profile_members AS p2 ON p1.locus=p2.locus AND 
-		p1.scheme_id=p2.scheme_id AND p1.scheme_id=i_scheme_id WHERE p1.profile_id=profile_id1 AND p1.profile_id!=p2.profile_id AND
-		(p1.allele_id=p2.allele_id OR p1.allele_id='N' OR p2.allele_id='N') AND 
-		p2.profile_id IN (SELECT profile_id FROM classification_group_profiles WHERE cg_scheme_id=i_cg_scheme_id) 
+		p1.scheme_id=p2.scheme_id AND p1.scheme_id=i_scheme_id JOIN classification_group_profiles cgp ON 
+		p2.profile_id=cgp.profile_id AND cgp.cg_scheme_id=i_cg_scheme_id WHERE p1.profile_id=profile_id1 
+		AND p1.profile_id!=p2.profile_id AND (p1.allele_id=p2.allele_id OR p1.allele_id='N' OR p2.allele_id='N')  
 		GROUP BY p2.profile_id HAVING COUNT(*) >= (locus_count-threshold);
 	END 
 $$ LANGUAGE plpgsql;
@@ -1396,9 +1396,9 @@ RETURNS setof text AS $$
 		COUNT(CASE WHEN p1.allele_id=p2.allele_id THEN 1 ELSE NULL END) AS matched,
 		ROUND((CAST(COUNT(*) AS float)*(total-i_threshold))/total) AS threshold 
 		FROM profile_members AS p1 JOIN profile_members AS p2 ON p1.locus=p2.locus AND p1.scheme_id=p2.scheme_id AND 
-		p1.scheme_id=i_scheme_id WHERE p1.profile_id=profile_id1 AND p1.profile_id!=p2.profile_id AND p1.allele_id!='N' 
-		AND p2.allele_id!='N' AND p2.profile_id IN (SELECT profile_id FROM classification_group_profiles WHERE 
-		cg_scheme_id=i_cg_scheme_id) GROUP BY p2.profile_id;
+		p1.scheme_id=i_scheme_id JOIN classification_group_profiles cgp ON p2.profile_id=cgp.profile_id AND 
+		cgp.cg_scheme_id=i_cg_scheme_id WHERE p1.profile_id=profile_id1 AND p1.profile_id!=p2.profile_id AND 
+		p1.allele_id!='N' AND p2.allele_id!='N' GROUP BY p2.profile_id;
 
 		RETURN QUERY SELECT profile_id FROM loci_in_common WHERE matched>=threshold;
 		DROP TABLE loci_in_common;	
--- sql/upgrade/seqdefdb_v1.21.sql
+++ sql/upgrade/seqdefdb_v1.21.sql
@@ -1,1 +1,40 @@
 CREATE INDEX i_s3 ON sequences USING brin(datestamp);
+
+CREATE OR REPLACE FUNCTION matching_profiles_cg(i_cg_scheme_id int, profile_id1 text, threshold int) 
+	RETURNS setof text AS $$
+	DECLARE
+		i_scheme_id int;
+		locus_count int;
+	BEGIN
+		SELECT cs.scheme_id INTO i_scheme_id FROM classification_schemes cs WHERE id=i_cg_scheme_id;	
+		SELECT COUNT(*) INTO locus_count FROM scheme_members WHERE scheme_id=i_scheme_id;	
+		RETURN QUERY SELECT p2.profile_id FROM profile_members AS p1 JOIN profile_members AS p2 ON p1.locus=p2.locus AND 
+		p1.scheme_id=p2.scheme_id AND p1.scheme_id=i_scheme_id JOIN classification_group_profiles cgp ON 
+		p2.profile_id=cgp.profile_id AND cgp.cg_scheme_id=i_cg_scheme_id WHERE p1.profile_id=profile_id1 
+		AND p1.profile_id!=p2.profile_id AND (p1.allele_id=p2.allele_id OR p1.allele_id='N' OR p2.allele_id='N')  
+		GROUP BY p2.profile_id HAVING COUNT(*) >= (locus_count-threshold);
+	END 
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION matching_profiles_with_relative_threshold_cg(i_cg_scheme_id int, profile_id1 text, i_threshold int) 
+RETURNS setof text AS $$
+	DECLARE
+		i_scheme_id int;
+		total int;
+	BEGIN
+		SELECT cs.scheme_id INTO i_scheme_id FROM classification_schemes cs WHERE id=i_cg_scheme_id;	
+		SELECT COUNT(*) INTO total FROM scheme_members WHERE scheme_id=i_scheme_id;
+		CREATE TEMP TABLE loci_in_common AS SELECT p2.profile_id AS profile_id,COUNT(*) AS loci,
+		COUNT(CASE WHEN p1.allele_id=p2.allele_id THEN 1 ELSE NULL END) AS matched,
+		ROUND((CAST(COUNT(*) AS float)*(total-i_threshold))/total) AS threshold 
+		FROM profile_members AS p1 JOIN profile_members AS p2 ON p1.locus=p2.locus AND p1.scheme_id=p2.scheme_id AND 
+		p1.scheme_id=i_scheme_id JOIN classification_group_profiles cgp ON p2.profile_id=cgp.profile_id AND 
+		cgp.cg_scheme_id=i_cg_scheme_id WHERE p1.profile_id=profile_id1 AND p1.profile_id!=p2.profile_id AND 
+		p1.allele_id!='N' AND p2.allele_id!='N' GROUP BY p2.profile_id;
+
+		RETURN QUERY SELECT profile_id FROM loci_in_common WHERE matched>=threshold;
+		DROP TABLE loci_in_common;	
+	END;
+$$ LANGUAGE plpgsql;
+
+
