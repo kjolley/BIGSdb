e077ce7606660e8afdfc9d6cdfa15d8316e50a58
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2018-11-22 11:53:24 +0000

    Add BRIN index on sequences(datestamp).

--- sql/seqdef.sql
+++ sql/seqdef.sql
@@ -337,6 +337,7 @@ ON UPDATE CASCADE
 
 CREATE UNIQUE INDEX i_s1 ON sequences(locus,md5(sequence));
 CREATE INDEX i_s2 ON sequences(exemplar) WHERE exemplar;
+CREATE INDEX i_s3 ON sequences USING brin(datestamp); 
 GRANT SELECT,UPDATE,INSERT,DELETE ON sequences TO apache;
 
 CREATE TABLE sequence_extended_attributes (
---
+++ sql/upgrade/seqdefdb_v1.21.sql
@@ -0,0 +1,1 @@
+CREATE INDEX i_s3 ON sequences USING brin(datestamp);
