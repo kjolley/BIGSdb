kjolley/BIGSdb

commit 44841cf044b9b2193e7e006c810cc6e1e824c5b6

Export: Fix warnings when no alleles defined for a locus.
Fixes #536.

lib/BIGSdb/Plugins/Export.pm
@@ -42,7 +42,7 @@ sub get_attributes {
 		buttontext  => 'Dataset',
 		menutext    => 'Export dataset',
 		module      => 'Export',
-		version     => '1.6.1',
+		version     => '1.6.2',
 		dbtype      => 'isolates',
 		section     => 'export,postquery',
 		url         => "$self->{'config'}->{'doclink'}/data_export.html#isolate-record-export",
@@ -620,7 +620,7 @@ sub _write_allele {
 	my ( $self, $args ) = @_;
 	my ( $fh, $locus, $data, $all_allele_ids, $first_col, $params ) =
 	  @{$args}{qw(fh locus data all_allele_ids first params)};
-	my @unsorted_allele_ids = defined $all_allele_ids->{$locus} ? @{ $all_allele_ids->{$locus} } : q();
+	my @unsorted_allele_ids = defined $all_allele_ids->{$locus} ? @{ $all_allele_ids->{$locus} } : ();
 	my $allele_ids = $self->_sort_alleles( $locus, \@unsorted_allele_ids );
 	if ( $params->{'alleles'} ) {
 		my $first_allele = 1;

commit 1752c87dbcbbbd24586c163a640397f78ccfff2e

Export: Fix error introduced with commit 44841cf044b9b2193e7e006c810cc6e1e824c5b6
Closes #536.

lib/BIGSdb/Plugins/Export.pm
@@ -616,7 +616,7 @@ sub _write_field {
 sub _sort_alleles {
 	my ( $self, $locus, $allele_ids ) = @_;
 	my $locus_info = $self->{'datastore'}->get_locus_info($locus);
-	return $allele_ids if !$locus_info;
+	return $allele_ids if !$locus_info || $allele_ids->[0] eq q();
 	my @list = $locus_info->{'allele_id_format'} eq 'integer' ? sort { $a <=> $b } @$allele_ids : sort @$allele_ids;
 	return \@list;
 }
@@ -625,7 +625,7 @@ sub _write_allele {
 	my ( $self, $args ) = @_;
 	my ( $fh, $locus, $data, $all_allele_ids, $first_col, $params ) =
 	  @{$args}{qw(fh locus data all_allele_ids first params)};
-	my @unsorted_allele_ids = defined $all_allele_ids->{$locus} ? @{ $all_allele_ids->{$locus} } : ();
+	my @unsorted_allele_ids = defined $all_allele_ids->{$locus} ? @{ $all_allele_ids->{$locus} } : (q());
 	my $allele_ids = $self->_sort_alleles( $locus, \@unsorted_allele_ids );
 	if ( $params->{'alleles'} ) {
 		my $first_allele = 1;