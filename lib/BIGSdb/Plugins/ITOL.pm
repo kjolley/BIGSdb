2b13f9dc4116017c24ddef5bd13486ec65ed5b82
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2019-02-20 10:08:23 +0000

    Formatting.

--- lib/BIGSdb/Plugins/ITOL.pm
+++ lib/BIGSdb/Plugins/ITOL.pm
@@ -365,12 +365,14 @@ sub generate_tree_files {
 			  ->update_job_output( $job_id, { filename => "$job_id.ph", description => '20_NJ tree (Newick format)' } );
 		} else {
 			$logger->error('Tree file has not been generated.');
-			if (!-e $self->{'config'}->{'clustalw_path'}){
+			if ( !-e $self->{'config'}->{'clustalw_path'} ) {
 				$logger->error("CLUSTALW program does not exist at $self->{'config'}->{'clustalw_path'}.");
-			} elsif (!-x $self->{'config'}->{'clustalw_path'}){
+			} elsif ( !-x $self->{'config'}->{'clustalw_path'} ) {
 				$logger->error("CLUSTALW program at $self->{'config'}->{'clustalw_path'} is not executable.");
 			} else {
-                                $logger->debug("CLUSTALW program has been found at $self->{'config'}->{'clustalw_path'} and is executable.");
+				$logger->debug(
+					"CLUSTALW program has been found at $self->{'config'}->{'clustalw_path'} and is executable.")
+				  ;
 			}
 			$failed = 1;
 		}
