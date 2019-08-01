4fdd9965d86722c50252da541e1b9a65cd2b3c98
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2019-08-01 13:50:35 +0100

    Version 1.23.0.

--- lib/BIGSdb/Application.pm
+++ lib/BIGSdb/Application.pm
@@ -20,7 +20,7 @@ package BIGSdb::Application;
 use strict;
 use warnings;
 use 5.010;
-use version; our $VERSION = version->declare('v1.22.5');
+use version; our $VERSION = version->declare('v1.23.0');
 use Apache2::Connection;
 use parent qw(BIGSdb::BaseApplication);
 use BIGSdb::AjaxJobs;
