a4389980c08d6f72499a5c578ac45a623983671e
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2020-10-09 14:53:43 +0100

    Record database curators in users database.

---
+++ sql/upgrade/users_v1.29.sql
@@ -0,0 +1,32 @@
+CREATE TABLE registered_curators (
+dbase_config text NOT NULL,
+user_name text NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY (dbase_config,user_name),
+CONSTRAINT rc_user_name FOREIGN KEY (user_name) REFERENCES users
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT rc_dbase_config FOREIGN KEY (dbase_config) REFERENCES registered_resources
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON registered_curators TO apache;
+
+CREATE TABLE submission_digest (
+user_name text NOT NULL,
+timestamp timestamp NOT NULL,
+dbase_description text NOT NULL,
+submission_id text NOT NULL,
+submitter text NOT NULL,
+summary text NOT NULL,
+PRIMARY KEY (user_name,timestamp),
+CONSTRAINT sd_user_name FOREIGN KEY (user_name) REFERENCES users
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON registered_curators TO apache;
+
+ALTER TABLE users ADD submission_digests boolean;
+ALTER TABLE users ADD absent_until date;
