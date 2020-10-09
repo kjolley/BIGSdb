0d9daf26d7a74a3e949060a5bc6b2d40ac561673
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2020-10-09 16:46:15 +0100

    Store submission summary in digest table.

--- lib/BIGSdb/SubmissionHandler.pm
+++ lib/BIGSdb/SubmissionHandler.pm
@@ -1658,6 +1658,24 @@ sub email {
 	return;
 }
 
+sub _get_digest_summary {
+	my ( $self, $submission_id, $options ) = @_;
+	my $submission = $self->get_submission($submission_id);
+	my %methods    = (
+		alleles  => '_get_allele_submission_summary',
+		profiles => '_get_profile_submission_summary',
+		isolates => '_get_isolate_submission_summary',
+		genomes  => '_get_isolate_submission_summary'
+	);
+	my $msg = q();
+	if ( $methods{ $submission->{'type'} } ) {
+		my $method = $methods{ $submission->{'type'} };
+		my $summary = $self->$method( $submission_id, { single_line => 1 } );
+		$msg = $summary if $summary;
+	}
+	return $msg;
+}
+
 sub get_text_summary {
 	my ( $self, $submission_id, $options ) = @_;
 	my $submission = $self->get_submission($submission_id);
@@ -1759,6 +1777,30 @@ sub notify_curators {
 	my $curators   = $self->_get_curators($submission_id);
 	foreach my $curator_id (@$curators) {
 		next if !$self->can_email_curator($curator_id);
+
+		#Check if curator has enabled submission digests and doesn't want to be emailed on every submission.
+		my $curator_username = $self->{'datastore'}->run_query( 'SELECT user_name FROM users WHERE id=?', $curator_id );
+		my $curator_info = $self->{'datastore'}->get_user_info_from_username($curator_username);
+		if ( $curator_info->{'submission_digests'} && $curator_info->{'user_db'} ) {
+			my $message        = $self->_get_digest_summary( $submission_id, { messages => 1 } );
+			my $submitter_name = $self->{'datastore'}->get_user_string( $submission->{'submitter'} );
+			my $user_db        = $self->{'datastore'}->get_user_db( $curator_info->{'user_db'} );
+			eval {
+				$user_db->do(
+					'INSERT INTO submission_digests (user_name,timestamp,dbase_description,submission_id,'
+					  . 'submitter,summary) VALUES (?,?,?,?,?,?)',
+					undef,           $curator_username, 'now', $self->{'system'}->{'description'}, $submission_id,
+					$submitter_name, $message
+				);
+			};
+			if ($@) {
+				$logger->error($@);
+				$user_db->rollback;
+			} else {
+				$user_db->commit;
+			}
+			next;
+		}
 		my $desc = $self->{'system'}->{'description'} || 'BIGSdb';
 		my $message = qq(This message has been sent to curators/admins of the $desc database with privileges )
 		  . qq(required to curate this submission.\n\n);
@@ -1838,16 +1880,20 @@ sub _translate_outcome {
 }
 
 sub _get_allele_submission_summary {    ## no critic (ProhibitUnusedPrivateSubroutines) #Called by dispatch table
-	my ( $self, $submission_id ) = @_;
+	my ( $self, $submission_id, $options ) = @_;
 	my $allele_submission = $self->get_allele_submission($submission_id);
 	return if !$allele_submission;
+	my $seq_count = scalar @{ $allele_submission->{'seqs'} };
+	if ( $options->{'single_line'} ) {
+		return qq(Locus: $allele_submission->{'locus'}; Technology: $allele_submission->{'technology'}; )
+		  . qq(Sequence count: $seq_count);
+	}
 	my $return_buffer = q();
 	$return_buffer .= $self->_get_text_heading( 'Data summary', { blank_line_before => 1 } );
 	$return_buffer .= "Locus: $allele_submission->{'locus'}\n";
 	$return_buffer .= "Technology: $allele_submission->{'technology'}\n";
-	$return_buffer .= 'Sequence count: ' . scalar @{ $allele_submission->{'seqs'} } . "\n";
+	$return_buffer .= "Sequence count: $seq_count\n";
 	my $buffer = q();
-
 	foreach my $seq ( @{ $allele_submission->{'seqs'} } ) {
 		next if $seq->{'status'} eq 'pending';
 		$buffer .= "$seq->{'seq_id'}: $seq->{'status'}";
@@ -1862,16 +1908,19 @@ sub _get_allele_submission_summary {    ## no critic (ProhibitUnusedPrivateSubro
 }
 
 sub _get_profile_submission_summary {    ## no critic (ProhibitUnusedPrivateSubroutines) #Called by dispatch table
-	my ( $self, $submission_id ) = @_;
+	my ( $self, $submission_id, $options ) = @_;
 	my $profile_submission = $self->get_profile_submission($submission_id);
 	return if !$profile_submission;
 	my $scheme_info = $self->{'datastore'}->get_scheme_info( $profile_submission->{'scheme_id'}, { get_pk => 1 } );
+	my $profile_count = @{ $profile_submission->{'profiles'} };
+	if ( $options->{'single_line'} ) {
+		return qq(Scheme: $scheme_info->{'name'}; Profiles:$profile_count);
+	}
 	my $return_buffer = q();
 	$return_buffer .= $self->_get_text_heading( 'Data summary', { blank_line_before => 1 } );
 	$return_buffer .= "Scheme: $scheme_info->{'name'}\n";
-	$return_buffer .= 'Profile count: ' . scalar @{ $profile_submission->{'profiles'} } . "\n";
+	$return_buffer .= "Profile count: $profile_count\n";
 	my $buffer = q();
-
 	foreach my $profile ( @{ $profile_submission->{'profiles'} } ) {
 		next if $profile->{'status'} eq 'pending';
 		$buffer .= "$profile->{'profile_id'}: $profile->{'status'}";
@@ -1886,10 +1935,14 @@ sub _get_profile_submission_summary {    ## no critic (ProhibitUnusedPrivateSubr
 }
 
 sub _get_isolate_submission_summary {    ## no critic (ProhibitUnusedPrivateSubroutines) #Called by dispatch table
-	my ( $self, $submission_id ) = @_;
+	my ( $self, $submission_id, $options ) = @_;
 	my $isolate_submission = $self->get_isolate_submission($submission_id);
+	my $isolate_count      = @{ $isolate_submission->{'isolates'} };
+	if ( $options->{'single_line'} ) {
+		return qq(Isolate count: $isolate_count);
+	}
 	my $return_buffer = $self->_get_text_heading( 'Data summary', { blank_line_before => 1 } );
-	$return_buffer .= 'Isolate count: ' . scalar @{ $isolate_submission->{'isolates'} } . "\n";
+	$return_buffer .= "Isolate count: $isolate_count\n";
 	return $return_buffer;
 }
 
--- sql/upgrade/users_v1.29.sql
+++ sql/upgrade/users_v1.29.sql
@@ -13,7 +13,7 @@ ON UPDATE CASCADE
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON registered_curators TO apache;
 
-CREATE TABLE submission_digest (
+CREATE TABLE submission_digests (
 user_name text NOT NULL,
 timestamp timestamp NOT NULL,
 dbase_description text NOT NULL,
@@ -26,7 +26,7 @@ ON DELETE CASCADE
 ON UPDATE CASCADE
 );
 
-GRANT SELECT,UPDATE,INSERT,DELETE ON registered_curators TO apache;
+GRANT SELECT,UPDATE,INSERT,DELETE ON submission_digests TO apache;
 
 ALTER TABLE users ADD submission_digests boolean;
 ALTER TABLE users ADD absent_until date;
