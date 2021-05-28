98902e4977ab2f76d243b22676902efd9be1ece0
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2021-05-28 09:41:46 +0100

    Delete analysis_results records if sequence bin changes
    This is performed by a database trigger.
    
    Closes #776.

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -552,6 +552,8 @@ BEGIN
  -- We only want the following to run once per transaction per isolate
  -- Not on addition of each contig.
 	PERFORM update_n50(NEW.isolate_id);
+	DELETE FROM last_run WHERE isolate_id=NEW.isolate_id;
+	DELETE FROM analysis_results WHERE isolate_id=NEW.isolate_id;
 	RETURN NULL; 
 END;
 $BODY$ LANGUAGE plpgsql;
