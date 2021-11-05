5dd048f01ac3cea1e8433a4fa8a7efaeeacf55fd
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2021-11-05 08:03:21 +0000

    Tweak message.

--- lib/BIGSdb/CurateAddPage.pm
+++ lib/BIGSdb/CurateAddPage.pm
@@ -509,7 +509,7 @@ sub _check_sequence_length {
 	my $q = $self->{'cgi'};
 	return if $q->param('ignore_length');
 	my $explain = q(If the sequence correctly aligns with the locus start and end points then you can still add it )
-	 . q(by going back and checking the 'Override sequence length check' checkbox before re-submitting.);
+	 . q(by checking the 'Override sequence length check' checkbox before re-submitting.);
 	if ( !$locus_info->{'length_varies'} && defined $locus_info->{'length'} && $locus_info->{'length'} != $length ) {
 		push @$problems, "Sequence is $length $units long but this locus is set as a standard "
 		  . "length of $locus_info->{'length'} $units. $explain";
