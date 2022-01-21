457de5faf42a532c472805e847672b5b705501d4
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2022-01-21 12:33:26 +0000

    Version 1.33.0.

--- lib/BIGSdb/Application.pm
+++ lib/BIGSdb/Application.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#(c) 2010-2021, University of Oxford
+#(c) 2010-2022, University of Oxford
 #E-mail: keith.jolley@zoo.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -20,7 +20,7 @@ package BIGSdb::Application;
 use strict;
 use warnings;
 use 5.010;
-use version; our $VERSION = version->declare('v1.32.2');
+use version; our $VERSION = version->declare('v1.33.0');
 use Apache2::Connection;
 use parent qw(BIGSdb::BaseApplication);
 use BIGSdb::AjaxAnalysis;
