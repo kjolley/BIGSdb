ca6f02f2da89411a3cc4539ce7a0b283ab5ce654
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2022-03-07 12:50:46 +0000

    Add new lincode tables to SQL setup scripts.

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -1912,3 +1912,38 @@ AS $$
 SELECT ARRAY(SELECT unnest($1) ORDER BY 1)
 $$;
 
+CREATE TABLE lincode_schemes (
+scheme_id int NOT NULL,
+thresholds text NOT NULL,
+maindisplay boolean NOT NULL,
+curator int NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY(scheme_id),
+CONSTRAINT ls_scheme_id FOREIGN KEY(scheme_id) REFERENCES schemes
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT ls_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON lincode_schemes TO apache;
+
+CREATE TABLE lincode_fields (
+scheme_id int NOT NULL,
+field text NOT NULL,
+type text NOT NULL,
+display_order int,
+maindisplay boolean NOT NULL,
+curator int NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY(scheme_id,field),
+CONSTRAINT lf_scheme_id FOREIGN KEY(scheme_id) REFERENCES lincode_schemes
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT lf_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON lincode_fields TO apache;
