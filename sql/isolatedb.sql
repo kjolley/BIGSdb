138118bc6d1fc429c6a3b4d6b92e8fbde163812b
Keith Jolley <keith.jolley@zoo.ox.ac.uk>
2022-03-30 07:56:06 +0100

    Add codon_tables table to isolatedb.sql.

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -1947,3 +1947,20 @@ ON UPDATE CASCADE
 );
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON lincode_fields TO apache;
+
+CREATE TABLE codon_tables (
+isolate_id int NOT NULL,
+codon_table int NOT NULL,
+curator int NOT NULL,
+datestamp date NOT NULL,
+PRIMARY KEY(isolate_id),
+CONSTRAINT ct_isolate_id FOREIGN KEY (isolate_id) REFERENCES isolates
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+CONSTRAINT ct_curator FOREIGN KEY (curator) REFERENCES users
+ON DELETE NO ACTION
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON codon_tables TO apache;
+
