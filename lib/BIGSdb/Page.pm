e0f3ff3d1a1d5af7e584b6d3e5cca8e7d92f0b26
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2023-04-29 08:54:56 +0100

    Bump css version number.

--- lib/BIGSdb/Page.pm
+++ lib/BIGSdb/Page.pm
@@ -672,7 +672,7 @@ sub _get_meta_data {
 sub _get_stylesheets {
 	my ($self)  = @_;
 	my $system  = $self->{'system'};
-	my $version = '20230428';
+	my $version = '20230429';
 	my @filenames;
 	push @filenames, q(dropzone.css)                                          if $self->{'dropzone'};
 	push @filenames, q(billboard.min.css)                                     if $self->{'billboard'};
