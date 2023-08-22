38c1db18ea6c11fb5e762c3d834b36e8801637ee
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2023-08-22 07:52:29 +0100

    Fix locus_stats issue due to new presence value 'P'.
    Fixes #921.

--- sql/seqdef.sql
+++ sql/seqdef.sql
@@ -1022,18 +1022,18 @@ CREATE OR REPLACE FUNCTION update_locus_stats() RETURNS TRIGGER AS $update_locus
 		current_datestamp date;
 		allele_length integer;
 	BEGIN
-		IF (TG_OP = 'DELETE' AND OLD.allele_id NOT IN ('0','N')) THEN
+		IF (TG_OP = 'DELETE' AND OLD.allele_id NOT IN ('0','N','P')) THEN
 			PERFORM locus FROM sequences WHERE locus=OLD.locus;
 			IF NOT FOUND THEN  --There are no more alleles for this locus.
 				UPDATE locus_stats SET datestamp=null,allele_count=0,min_length=null,max_length=null WHERE locus=OLD.locus;
 			ELSE
 				SELECT MIN(LENGTH(sequence)),MAX(LENGTH(sequence)),MAX(datestamp) INTO 
 				current_min_length,current_max_length,current_datestamp FROM sequences WHERE 
-				locus=OLD.locus AND allele_id NOT IN ('0','N');
+				locus=OLD.locus AND allele_id NOT IN ('0','N','P');
 				UPDATE locus_stats SET datestamp=current_datestamp,allele_count=allele_count-1,
 				min_length=current_min_length,max_length=current_max_length WHERE locus=OLD.locus;
 			END IF;
-		ELSIF (TG_OP = 'INSERT' AND NEW.allele_id NOT IN ('0','N')) THEN
+		ELSIF (TG_OP = 'INSERT' AND NEW.allele_id NOT IN ('0','N','P')) THEN
 			UPDATE locus_stats SET datestamp='now',allele_count=allele_count+1 WHERE locus=NEW.locus;
 			SELECT min_length,max_length INTO current_min_length,current_max_length FROM locus_stats WHERE locus=NEW.locus;
 			allele_length := LENGTH(NEW.sequence);
