kjolley/BIGSdb

commit d4b5439c74c2ee381ccea799bc988d7a1f360e90

Log access from curator interface.
Add log table to auth.sql.

lib/BIGSdb/IsolateQueryPage.pm
@@ -284,9 +284,7 @@ sub _print_interface {
 sub print_panel_buttons {
 	my ($self) = @_;
 	my $q = $self->{'cgi'};
-	if (   !defined $q->param('currentpage')
-		|| ( defined $q->param('pagejump') && $q->param('pagejump') eq '1' )
-		|| $q->param('First') )
+	if ( $self->_showing_first_page )
 	{
 		say q(<span class="icon_button">)
 		  . q(<a class="trigger_button" id="panel_trigger" style="display:none">)
@@ -4304,13 +4302,11 @@ sub _highest_entered_fields {
 sub _showing_first_page {
 	my ($self) = @_;
 	my $q = $self->{'cgi'};
-	if (
-		( $q->param('submit') || $q->param('sent') || defined $q->param('query_file') )
+	if (   ( $q->param('submit') || $q->param('sent') || defined $q->param('query_file') )
 		&& !$q->param('pagejump')
 		&& !$q->param('Last')
 		&& !$q->param('>')
-		&& !( $q->param('<') && ( $q->param('currentpage') // q() ) ne '2' )
-	  )
+		&& !( $q->param('<') && ( $q->param('currentpage') // q() ) ne '2' ) )
 	{
 		return 1;
 	}

lib/BIGSdb/Application.pm
@@ -584,25 +584,32 @@ sub print_page {
 	if ( $page_attributes{'error'} ) {
 		$self->{'handled_error'} = 1;
 	}
-	$self->_log_call;
+	$self->log_call;
 	return;
 }
 
-sub _log_call {
-	my ($self) = @_;
+sub log_call {
+	my ( $self, $options ) = @_;
 	return if !$self->{'config'}->{'web_log_to_db'};
 	my $q = $self->{'cgi'};
 	return if $q->param('ajax');    #We don't want to log every AJAX update on dashboard for instance.
-	my $page = $self->{'page'};
-	if ($page eq 'plugin' && defined $q->param('name')){
+	return if $q->param('no_header'); 
+	my $page   = $self->{'page'};
+	my %ignore = map { $_ => 1 } qw(ajaxAnalysis);
+	return if $ignore{$page};
+	if ( $page eq 'plugin' && defined $q->param('name') ) {
 		my $name = $q->param('name');
 		$page = "plugin [$name]";
+	} elsif ($q->param('table')){
+		my $table = $q->param('table');
+		$page = "$page [$table]";
 	}
 	eval {
-		$self->{'auth_db'}
-		  ->do( 'INSERT INTO log (timestamp,ip_address,user_name,curate,method,instance,page) VALUES (?,?,?,?,?,?,?)',
-			undef, 'now', $ENV{'REMOTE_ADDR'}, $self->{'username'}, 'false', $q->request_method, $self->{'instance'},
-			$page );
+		$self->{'auth_db'}->do(
+			'INSERT INTO log (timestamp,ip_address,user_name,curate,method,instance,page) VALUES (?,?,?,?,?,?,?)',
+			undef, 'now', $ENV{'REMOTE_ADDR'}, $self->{'username'}, ( $options->{'curate'} ? 'true' : 'false' ),
+			$q->request_method, $self->{'instance'}, $page
+		);
 	};
 	if ($@) {
 		$logger->error("Cannot log page access. $@");

lib/BIGSdb/Curate.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#Copyright (c) 2010-2022, University of Oxford
+#Copyright (c) 2010-2024, University of Oxford
 #E-mail: keith.jolley@biology.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -179,6 +179,7 @@ sub print_page {
 	my $page;
 	my $continue = 1;
 	my $auth_cookies_ref;
+
 	if ( $self->{'error'} ) {
 		$page_attributes{'error'}              = $self->{'error'};
 		$page_attributes{'max_upload_size_mb'} = $self->{'max_upload_size_mb'};
@@ -258,6 +259,7 @@ sub print_page {
 		}
 		$page = "BIGSdb::$classes{$self->{'page'}}"->new(%page_attributes);
 		$page->print_page_content;
+		$self->log_call( { curate => 1 } );
 		return;
 	}
 	$page_attributes{'error'} = 'unknown';
@@ -266,6 +268,7 @@ sub print_page {
 	if ( $page_attributes{'error'} ) {
 		$self->{'handled_error'} = 1;
 	}
+	
 	return;
 }
 1;

sql/auth.sql
@@ -122,3 +122,15 @@ ON UPDATE CASCADE
 
 GRANT SELECT,UPDATE,DELETE,INSERT ON api_sessions TO apache;
 
+CREATE UNLOGGED TABLE log (
+timestamp timestamp NOT NULL,
+ip_address text NOT NULL,
+user_name text,
+curate boolean NOT NULL,
+method text NOT NULL,
+instance text NOT NULL,
+page text NOT NULL
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON log TO apache;
+CREATE INDEX l_l1 ON log USING brin(timestamp);

sql/upgrade/auth_v1.43.sql
@@ -8,5 +8,5 @@ instance text NOT NULL,
 page text NOT NULL
 );
 
-GRANT SELECT,UPDATE,INSERT,DELETE ON log TO apache,bigsdb;
+GRANT SELECT,UPDATE,INSERT,DELETE ON log TO apache;
 CREATE INDEX l_l1 ON log USING brin(timestamp);

commit 36997dc456ed2564ad0580abe968223d3e93e865

Fix display of 'Modify form' trigger on isolate query page.
Broken in commit d4b5439.

lib/BIGSdb/IsolateQueryPage.pm
@@ -284,7 +284,11 @@ sub _print_interface {
 sub print_panel_buttons {
 	my ($self) = @_;
 	my $q = $self->{'cgi'};
-	if ( $self->_showing_first_page )
+	if (
+		!defined $q->param('currentpage')
+		|| ( defined $q->param('pagejump') && $q->param('pagejump') eq '1' )
+		|| $q->param('First')
+	  )
 	{
 		say q(<span class="icon_button">)
 		  . q(<a class="trigger_button" id="panel_trigger" style="display:none">)