b51aa0db4aad44aeebf231be959dc14deec7244a
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2024-02-01 13:45:18 +0000

    Use case-insensitive check for 'null'.

--- lib/BIGSdb/Offline/BatchUploader.pm
+++ lib/BIGSdb/Offline/BatchUploader.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#Copyright (c) 2021-2022, University of Oxford
+#Copyright (c) 2021-2024, University of Oxford
 #E-mail: keith.jolley@biology.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -268,7 +268,7 @@ sub _process_fields {
 		$value =~ s/\s+$//x;
 		$value =~ s/\r//gx;
 		$value =~ s/\n/ /gx;
-		$value =~ s/^null$//gx if !$options->{'allow_null'};
+		$value =~ s/^null$//gxi if !$options->{'allow_null'};
 		if ( $value eq q() ) {
 			push @return_data, undef;
 		} else {
