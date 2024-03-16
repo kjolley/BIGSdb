e6189b72555f115a7c35299d4964ec4df210e0e0
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2024-03-16 16:31:08 +0000

    Make sure field add button doesn't expand in height.

--- lib/BIGSdb/Page.pm
+++ lib/BIGSdb/Page.pm
@@ -704,7 +704,7 @@ sub _get_meta_data {
 sub _get_stylesheets {
 	my ($self)  = @_;
 	my $system  = $self->{'system'};
-	my $version = '20240313';
+	my $version = '20240316';
 	my @filenames;
 	push @filenames, q(dropzone.css)                                          if $self->{'dropzone'};
 	push @filenames, q(billboard.min.css)                                     if $self->{'billboard'};
