7e0617d518c6d8e164b382ce2ea42b41eb34ddb1
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2024-04-16 14:34:12 +0100

    Prevent case ambiguity in user_name when registering.
    Closes #795.

--- lib/BIGSdb/UserRegistrationPage.pm
+++ lib/BIGSdb/UserRegistrationPage.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#Copyright (c) 2016-2022, University of Oxford
+#Copyright (c) 2016-2024, University of Oxford
 #E-mail: keith.jolley@biology.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
@@ -488,8 +488,8 @@ sub _bad_username {
 	my $invalid =
 	  $self->{'datastore'}->run_query( 'SELECT user_name FROM invalid_usernames UNION SELECT user_name FROM users',
 		undef, { fetch => 'col_arrayref' } );
-	my %invalid = map { $_ => 1 } @$invalid;
-	if ( $invalid{$user_name} ) {
+	my %invalid = map { uc($_) => 1 } @$invalid;
+	if ( $invalid{ uc $user_name } ) {
 		push @problems, q(Username is already registered. Site-wide accounts cannot use a user name )
 		  . q(that is currently in use in any databases on the site.);
 	}
