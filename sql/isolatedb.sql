37e1f21ef9022ad464472e66c317de7e4c5abd0a
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2024-04-30 09:30:44 +0100

    Update db version.

--- sql/isolatedb.sql
+++ sql/isolatedb.sql
@@ -2087,7 +2087,7 @@ PRIMARY KEY(field)
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON db_attributes TO apache;
 
-INSERT INTO db_attributes (field,value) VALUES ('version','45');
+INSERT INTO db_attributes (field,value) VALUES ('version','46');
 INSERT INTO db_attributes (field,value) VALUES ('type','isolates');
 
 CREATE TABLE query_interfaces (
