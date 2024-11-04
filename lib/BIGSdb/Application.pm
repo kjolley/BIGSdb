98a20b2df152f0c5d21c8f62afe7bb23d4570cc7
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2024-11-04 09:10:04 +0000

    Version 1.49.0.

--- lib/BIGSdb/Application.pm
+++ lib/BIGSdb/Application.pm
@@ -20,7 +20,7 @@ package BIGSdb::Application;
 use strict;
 use warnings;
 use 5.010;
-use version; our $VERSION = version->declare('v1.48.5');
+use version; our $VERSION = version->declare('v1.49.0');
 use Apache2::Connection;
 use parent qw(BIGSdb::BaseApplication);
 use BIGSdb::AjaxAnalysis;
