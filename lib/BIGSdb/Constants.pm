b29fb9c9eb732f9117f6867c2ed4a624194fe2ef
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2025-01-24 12:02:36 +0000

    Update year.

--- lib/BIGSdb/Constants.pm
+++ lib/BIGSdb/Constants.pm
@@ -1,5 +1,5 @@
 #Written by Keith Jolley
-#Copyright (c) 2015-2024, University of Oxford
+#Copyright (c) 2015-2025, University of Oxford
 #E-mail: keith.jolley@biology.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
