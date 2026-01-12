14dc20b6584cca6a6f1632aae6d612a80d920a86
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2026-01-12 09:41:41 +0000

    Add locus foreign key on mutation tables.
    Fixes #1079.

---
+++ sql/upgrade/seqdefdb_v1.52.sql
@@ -0,0 +1,14 @@
+UPDATE db_attributes SET value='52' WHERE field='version';
+
+ALTER TABLE dna_mutations ADD CONSTRAINT dm_wild_type_allele_id 
+ FOREIGN KEY (locus,wild_type_allele_id) REFERENCES sequences(locus,allele_id)
+ ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE dna_mutations ADD CONSTRAINT dm_locus 
+ FOREIGN KEY (locus) REFERENCES loci 
+ ON DELETE CASCADE ON UPDATE CASCADE;
+ 
+ALTER TABLE peptide_mutations ADD CONSTRAINT pm_locus 
+ FOREIGN KEY (locus) REFERENCES loci
+ ON DELETE CASCADE
+ ON UPDATE CASCADE;
+ 
