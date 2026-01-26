db7da2f76a5b412a1d74a905740d422f74b71992
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2026-01-26 10:43:38 +0000

    Update version number in db_attributes.

--- sql/seqdef.sql
+++ sql/seqdef.sql
@@ -1776,5 +1776,5 @@ PRIMARY KEY(field)
 
 GRANT SELECT,UPDATE,INSERT,DELETE ON db_attributes TO apache;
 
-INSERT INTO db_attributes (field,value) VALUES ('version','45');
+INSERT INTO db_attributes (field,value) VALUES ('version','52');
 INSERT INTO db_attributes (field,value) VALUES ('type','seqdef');
