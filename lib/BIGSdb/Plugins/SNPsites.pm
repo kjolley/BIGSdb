0a9bde64d7a52f18240298cf5e8d41a20b53e117
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2026-02-23 10:59:52 +0000

    Add system flag to Polymorphisms plugin.

--- lib/BIGSdb/Plugins/SNPsites.pm
+++ lib/BIGSdb/Plugins/SNPsites.pm
@@ -1,6 +1,6 @@
 #SNPsites.pm - Wrapper for snp-sites plugin for BIGSdb
 #Written by Keith Jolley
-#Copyright (c) 2024-2025, University of Oxford
+#Copyright (c) 2024-2026, University of Oxford
 #E-mail: keith.jolley@biology.ox.ac.uk
 #
 #This file is part of Bacterial Isolate Genome Sequence Database (BIGSdb).
