d822fdcb5285373e2dd218805e5d0f48bda9f5ec
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2026-03-02 11:11:25 +0000

    Version 1.52.1.

--- lib/BIGSdb/Page.pm
+++ lib/BIGSdb/Page.pm
@@ -227,7 +227,7 @@ sub _get_javascript_paths {
 			  { src => [qw(bigsdb.dataexplorer.min.js d3.v6.min.js)], defer => 1, version => '20230310' }
 		};
 		if ( $self->{'pluginJS'} ) {
-			$features->{'pluginJS'} = { src => ["Plugins/$self->{'pluginJS'}"], defer => 1, version => '20260226a' };
+			$features->{'pluginJS'} = { src => ["Plugins/$self->{'pluginJS'}"], defer => 1, version => '20260302' };
 		}
 		my %used;
 		foreach my $feature ( keys %$features ) {
