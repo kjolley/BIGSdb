dab91f00219ef233960c06eea33788323840209f
Keith Jolley <keith.jolley@biology.ox.ac.uk>
2026-05-20 10:34:42 +0100

    Add expand/contract button to isolate info page.

--- lib/BIGSdb/IsolateInfoPage.pm
+++ lib/BIGSdb/IsolateInfoPage.pm
@@ -56,7 +56,7 @@ sub initiate {
 		$self->{'noCache'} = 1;
 		return;
 	}
-	$self->{$_} = 1 foreach qw(jQuery jQuery.jstree jQuery.columnizer);
+	$self->{$_} = 1 foreach qw(jQuery jQuery.jstree jQuery.columnizer allowExpand);
 	my $field_attributes = $self->{'xmlHandler'}->get_all_field_attributes;
 	foreach my $field ( keys %$field_attributes ) {
 		if ( $field_attributes->{$field}->{'type'} eq 'geography_point'
